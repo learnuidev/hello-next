332c4ae830303ee9fa0b5334dd8901b69f6e3019
learnuidev <learnuidev@gmail.com>
2023-09-29 22:57:22 -0400

    Added stick footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@ import { QueryClientProvider } from "@/libs/react-query";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBuilding,
+  faBuildingUser,
+  faScrewdriverWrench,
+  faToolbox,
+} from "@fortawesome/sharp-solid-svg-icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +25,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-bkg text-content`}>
-        <QueryClientProvider>{children}</QueryClientProvider>
+      <body
+        className={`${inter.className} bg-bkg text-content flex h-screen flex-col`}
+      >
+        <div className="flex-1">
+          <QueryClientProvider>{children}</QueryClientProvider>
+        </div>
+
+        <footer className="font-light text-xs my-4 flex justify-center items-center space-x-2 text-gray-300">
+          <FontAwesomeIcon icon={faScrewdriverWrench} />
+          <Link
+            target="_blank"
+            href="https://www.linkedin.com/in/vishalgautamm/"
+          >
+            Vishal Gautam
+          </Link>
+        </footer>
       </body>
     </html>
   );

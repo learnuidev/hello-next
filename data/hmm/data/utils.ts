818ed61ce53ea59a72dd5b3483c97056d69b73f6
learnuidev <learnuidev@gmail.com>
2023-10-25 12:40:55 -0400

    wip

---
+++ data/hmm/data/utils.ts
@@ -0,0 +1,22 @@
+export const characterTonesMap = {
+  1: "āēīōū",
+  2: "áéíóú",
+  3: "ǎěǐǒǔ",
+  4: "ǎèìòù",
+};
+
+export type ICharacter = {
+  hanzi: string;
+  pinyin: string;
+  en?: string;
+};
+
+export const getCharacterToneLevel = (character: ICharacter): number | undefined => {
+  const level = Object.entries(characterTonesMap)?.find(([tone, vals]) => {
+    return vals?.split("")?.find((v) => character?.pinyin?.includes(v));
+  })?.[0];
+
+  if (level) {
+    return parseInt(level);
+  }
+};

aa0ac827975cde79b902133dbab51f31ec26d713
learnuidev <learnuidev@gmail.com>
2024-04-01 16:23:30 -0400

    wip

--- components/selected-character.tsx
+++ components/selected-character.tsx
@@ -13,6 +13,7 @@ import {
   faHome,
   faLanguage,
   faLightbulb,
+  faMagnifyingGlass,
   faMale,
   faSpaceStationMoon,
   faXmark,
@@ -310,6 +311,12 @@ export function SelectedCharacter({ characterId }: { characterId: string }) {
             >
               <FontAwesomeIcon icon={faGoogle} />
             </Link>
+            <Link
+              href={`/search?query=${encodeURIComponent(currentPhrase?.hanzi)}`}
+              className={`text-sm bg-white dark:bg-black p-2 w-8 h-8 ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`}
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Link>
           </div>
         </div>
       );
@@ -371,6 +378,12 @@ export function SelectedCharacter({ characterId }: { characterId: string }) {
             className={`text-sm bg-white dark:bg-black p-2 w-8 h-8 ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`}
           >
             <FontAwesomeIcon icon={faGoogle} />
+          </Link>
+          <Link
+            href={`/search?query=${encodeURIComponent(currentPhrase?.hanzi)}`}
+            className={`text-sm bg-white dark:bg-black p-2 w-8 h-8 ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`}
+          >
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
           </Link>
         </div>
       </div>

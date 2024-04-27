2493f5b0f13221839ce286ec9336808b318c0f5b
learnuidev <learnuidev@gmail.com>
2024-04-27 00:33:06 -0400

    wip

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -32,7 +32,7 @@ export const SearchBar = () => {
 
       <input
         className="font-extralight border-none dark:placeholder:text-gray-500 border-gray-100 focus:border-gray-300 dark:bg-black/10 dark:text-gray-300 placeholder:text-gray-400 opacity-100 border-2 w-[140px] md:w-[500px] focus:border-none px-2 rounded-full focus:outline-none active:outline-none"
-        placeholder={"ask me anything"}
+        placeholder={"search"}
         onChange={(event) => {
           setQuery(() => event?.target?.value);
         }}

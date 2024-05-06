5d494322f8930bb5a0fa8f7e491e41a037b39300
learnuidev <learnuidev@gmail.com>
2024-05-05 22:20:51 -0400

    wip

--- components/word-item.tsx
+++ components/word-item.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Link from "next/link";
 
 import { useListCharactersQuery } from "@/domain/lesson/character.queries";
+import { cn } from "@/lib/utils";
 
 export const WordItem = ({ component: prop }: { component: any }) => {
   const { data: characters } = useListCharactersQuery();
@@ -18,8 +19,13 @@ export const WordItem = ({ component: prop }: { component: any }) => {
       key={JSON.stringify(prop)}
       className={`${character ? "dark:text-gray-400 text-gray-200" : "dark:text-gray-500 text-gray-200"} dark:hover:text-white p-6 flex items-center flex-col`}
     >
-      <span className="block p-0 m-0 text-sm">
-        {prop?.roman || character?.roman}
+      <span
+        className={cn(
+          "block p-0 m-0 text-sm",
+          prop?.roman || character?.roman ? "" : "text-black"
+        )}
+      >
+        {prop?.roman || character?.roman || "yo"}
       </span>
       <span className="text-2xl"> {prop.input}</span>
       <span className="block text-sm"> {character?.en || prop.en}</span>

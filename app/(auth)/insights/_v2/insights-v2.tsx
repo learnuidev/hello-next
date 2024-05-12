e521926290992dd92c96e7f070ad39902bc6d2e8
learnuidev <learnuidev@gmail.com>
2024-05-12 19:47:30 -0400

    wip

--- app/(auth)/insights/_v2/insights-v2.tsx
+++ app/(auth)/insights/_v2/insights-v2.tsx
@@ -10,7 +10,11 @@ import {
   VictoryAxis,
   VictoryLabel,
   Bar,
+  Axis,
+  LineSegment,
 } from "victory";
+import { cn } from "@/lib/utils";
+import { belts } from "@/app/nmm/utils";
 
 const humanLangs = {
   ne: "Nepali",
@@ -64,12 +68,8 @@ export const InsightsV2 = () => {
     data: { fill: "tomato" },
   };
 
-  return (
-    <div className="m-auto flex flex-col w-full justify-center">
-      <h1 className="text-center w-full mb-[-100px] mt-8 text-4xl font-extralight">
-        {" "}
-        Stats
-      </h1>
+  const VictoryChartVersion = () => {
+    return (
       <div className="">
         <div className="w-full">
           <VictoryChart
@@ -82,56 +82,158 @@ export const InsightsV2 = () => {
               style={{
                 tickLabels: { fontSize: 5, fill: "white" },
               }}
-              events={[
-                {
-                  target: "tickLabels",
-                  //   mutation: () => {},
-                  eventHandlers: {
-                    onClick: (data) => {
-                      //   alert("YO", data);
-                      return [
-                        {
-                          target: "labels",
-                          mutation: (props) => {
-                            return props.text === "clicked"
-                              ? null
-                              : { text: "clicked" };
-                          },
-                        },
-                      ];
+              axisComponent={
+                <LineSegment
+                  events={{
+                    onClick: (event: any) => {
+                      //   alert("yo", event);
+                      //   alert("axis", JSON.stringify(event));
                     },
-                  },
-                },
-              ]}
+                  }}
+                />
+              }
+              //   events={[
+              //     {
+              //       target: "tickLabels",
+              //       //   mutation: () => {},
+              //       eventHandlers: {
+              //         onClick: (data) => {
+              //           //   alert("YO", data);
+              //           return [
+              //             {
+              //               target: "labels",
+              //               mutation: (props) => {
+              //                 return props.text === "clicked"
+              //                   ? null
+              //                   : { text: "clicked" };
+              //               },
+              //             },
+              //           ];
+              //         },
+              //       },
+              //     },
+              //   ]}
             />
             <VictoryBar
               //   padding={20}
               barWidth={({ index }) => 10}
-              dataComponent={<Bar />}
+              dataComponent={
+                <Bar
+                  events={{
+                    onClick: (event: any) => {
+                      console.log(event);
+                      //   alert(event);
+                    },
+                  }}
+                />
+              }
               style={style}
               data={dataA}
-              events={[
-                {
-                  target: "data",
-                  eventHandlers: {
-                    onClick: (data) => {
-                      alert("YO");
-                      return [
-                        {
-                          target: "labels",
-                          mutation: (props) => {
-                            return props.text === "clicked"
-                              ? null
-                              : { text: "clicked" };
-                          },
-                        },
-                      ];
-                    },
-                  },
-                },
-              ]}
             />
           </VictoryChart>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className="mx-4 md:mx-24 flex flex-col justify-start items-center w-full">
+      <h1 className="w-full my-8 text-4xl font-extralight"> Stats</h1>
+
+      <div className="mx-4 mt-8">
+        <div className="space-y-2">
+          {Object.entries(grouped)
+            // .filter((group) => {
+            //   const [lang, items] = group;
+
+            //   return items?.length > 30;
+            // })
+            .map((group) => {
+              const [lang, items] = group;
+
+              const itemLen = items?.length || 0;
+
+              const x = humanLangs[lang] || lang;
+
+              const belt = belts?.filter(
+                (belt) =>
+                  belt.minCharacterLevel < itemLen &&
+                  itemLen < belt.maxCharacterLevel
+              )[0];
+
+              const barHeight = "h-6";
+
+              const percentage =
+                (((items || [])?.length * 1) / belt?.maxCharacterLevel) * 100;
+
+              return (
+                <div key={"lang"}>
+                  <div className="grid grid-cols-12">
+                    <p className="text-left text-gray-300 font-extralight text-sm">
+                      {x}
+                    </p>
+
+                    <div className="w-[320px] relative">
+                      <div
+                        className={cn(
+                          "w-full bg-gray-200 rounded-none dark:bg-black",
+                          barHeight
+                        )}
+                      >
+                        <div
+                          className={cn(
+                            belt?.fill || "bg-orange-600",
+                            "rounded-none",
+                            barHeight
+                          )}
+                          style={{
+                            width: `${percentage}%`,
+                          }}
+                        ></div>
+                      </div>
+
+                      <div className="text-left absolute z-50 top-[1px]  right-0">
+                        {percentage.toFixed(1)}%
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+
+              return (
+                <div
+                  key={lang}
+                  className="flex items-center justify-center mt-12"
+                >
+                  <div>
+                    <p className="text-left text-gray-300 font-extralight text-sm">
+                      {x}
+                    </p>
+                    <div className="w-[400px]">
+                      <div
+                        className={cn(
+                          "w-full bg-gray-200 rounded-none dark:bg-black",
+                          barHeight
+                        )}
+                      >
+                        <div
+                          className={cn(
+                            belt?.fill || "bg-orange-600",
+                            "rounded-none",
+                            barHeight
+                          )}
+                          style={{
+                            width: `${percentage}%`,
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="text-left">{percentage.toFixed(1)}%</div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>

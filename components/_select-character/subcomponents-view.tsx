45e9bceb5f677c43a259f2cc539e6d84ca71a4ad
learnuidev <learnuidev@gmail.com>
2024-05-22 14:24:10 -0400

    wip

--- components/_select-character/subcomponents-view.tsx
+++ components/_select-character/subcomponents-view.tsx
@@ -8,6 +8,10 @@ import { Skeleton } from "../ui/skeleton";
 import { persianAlphabets } from "@/langs/persian/persian-alphabets";
 import { urduAlphabets } from "@/langs/urdu/urdu-alphabets";
 import { russianAlphabets } from "@/langs/russian/russian-alphabets";
+import { hiraganaAlphabets } from "@/langs/japanese/hiragana-alphabets";
+import { japaneseComponents2 } from "@/langs/japanese/japanese-components";
+import { koreanAlphabets } from "@/langs/korean/korean-alphabets";
+import { koreanComponents2 } from "@/langs/korean/korean-components";
 
 interface SelectedCharacterProps {
   characterId: string;
@@ -133,6 +137,67 @@ const RussianSubComponentView = ({
     </div>
   );
 };
+const JapaneseSubComponentView = ({
+  characterId,
+  lang,
+}: SelectedCharacterProps) => {
+  const subComponents = characterId.split("")?.map((comp: any) => {
+    const alphabet = [...hiraganaAlphabets, ...japaneseComponents2].find(
+      (item) => item?.input === comp
+    );
+    return {
+      ...alphabet,
+    };
+  });
+  return (
+    <div className="text-gray-500 flex space-x-4 my-8">
+      {/* {JSON.stringify(sub_components, null, 2)} */}
+      {subComponents?.map((item: any) => {
+        return (
+          <Link
+            key={item?.input}
+            className="space-x-2 flex"
+            href={`/nmm/${item?.input}?lang=${lang}`}
+          >
+            <p>{item?.input}</p>
+            <p className="text-gray-400">{item?.roman}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+const KoreanSubComponentView = ({
+  characterId,
+  lang,
+}: SelectedCharacterProps) => {
+  const subComponents = characterId.split("")?.map((comp: any) => {
+    const alphabet = [...koreanAlphabets, ...koreanComponents2].find(
+      (item) => item?.input === comp
+    );
+    return {
+      ...alphabet,
+    };
+  });
+  return (
+    <div className="text-gray-500 flex space-x-4 my-8">
+      {/* {JSON.stringify(sub_components, null, 2)} */}
+      {subComponents?.map((item: any) => {
+        return (
+          <Link
+            key={item?.input}
+            className="space-x-2 flex"
+            href={`/nmm/${item?.input}?lang=${lang}`}
+          >
+            <p>{item?.input}</p>
+            <p className="text-gray-400">{item?.roman}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
 export const SubComponentsView = (props: SelectedCharacterProps) => {
   if (props.lang === "zh") {
@@ -148,6 +213,12 @@ export const SubComponentsView = (props: SelectedCharacterProps) => {
   }
   if (props.lang === "ru") {
     return <RussianSubComponentView {...props} />;
+  }
+  if (props.lang === "ja") {
+    return <JapaneseSubComponentView {...props} />;
+  }
+  if (props.lang === "ko") {
+    return <KoreanSubComponentView {...props} />;
   }
 
   return null;

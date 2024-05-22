f91676e8729895d00bf14ee35b205e13ece8b616
learnuidev <learnuidev@gmail.com>
2024-05-22 13:28:09 -0400

    added back auto focus

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -13,7 +13,7 @@ export const SearchInput = () => {
 
   return (
     <input
-      // autoFocus
+      autoFocus
       className="font-extralight border-none dark:placeholder:text-gray-500 border-gray-100 focus:border-gray-300 dark:bg-black/10 dark:text-gray-300 placeholder:text-gray-400 opacity-100 border-2 w-[140px] md:w-[500px] focus:border-none px-2 rounded-full focus:outline-none active:outline-none"
       placeholder={"Search"}
       onChange={handleOnChange}

001a91ef81e5b39f74d4cc86b89f3bab1b38daee
learnuidev <learnuidev@gmail.com>
2024-05-23 13:11:58 -0400

    remove test data

--- libs/dynamodb/utils/transform-item.ts
+++ libs/dynamodb/utils/transform-item.ts
@@ -10,7 +10,6 @@
 
 type DynamoDBItemMap = any;
 
-let counter = 0;
 export const transformItem = (item: DynamoDBItemMap): Record<string, any> => {
   // If the key is of type "M", then we simply flatten its valus and return it
   // This is done by calling the transformItem which takes a Map value
@@ -20,14 +19,6 @@ export const transformItem = (item: DynamoDBItemMap): Record<string, any> => {
     const mapValue = Object.values(item)?.[0] as DynamoDBItemMap;
     return transformItem(mapValue);
   }
-
-  counter++;
-
-  console.log("-------------------------");
-  console.log("OBJECT KEY", objKey);
-  console.log("COUNTER", counter);
-  console.log("ITEM", item);
-  console.log("-------------------------");
 
   // Other wise we loop over each key value pairs
   return Object.entries(item).reduce((acc, curr: any) => {
@@ -82,96 +73,3 @@ export const transformItem = (item: DynamoDBItemMap): Record<string, any> => {
   }, {});
   return item;
 };
-
-const sampleResponse = {
-  summary: {
-    S: 'The word "primavera" in Spanish refers to the season of spring. This season typically occurs between the months of March and June in the northern hemisphere, and between September and December in the southern hemisphere. During spring, temperatures begin to rise, flowers bloom, and nature comes back to life after the winter. It is a time of renewal and growth in the natural world. In Spanish-speaking countries, "primavera" is often celebrated with festivals, outdoor activities, and traditions that mark the arrival of this beautiful season.',
-  },
-  sentenceId: {
-    S: "primavera.",
-  },
-  creator: {
-    S: "learnuidev@gmail.com",
-  },
-  meanings: {
-    L: [
-      {
-        M: {
-          explanation: {
-            S: "This meaning refers to the verb 'ser' (to be) in Spanish, specifically when used to indicate that a subject is the same as the predicate.",
-          },
-          en: {
-            S: "is (third person singular present tense of the verb 'to be' in Spanish)",
-          },
-          original: {
-            S: "es (verbo ser en tercera persona del presente en español)",
-          },
-        },
-      },
-      {
-        M: {
-          explanation: {
-            S: "This meaning refers to the international standard language code for Spanish, which is 'es' under the ISO 639-1 standard.",
-          },
-          en: {
-            S: "es (ISO 639-1 code for the Spanish language)",
-          },
-          original: {
-            S: "es (código ISO 639-1 para el idioma español)",
-          },
-        },
-      },
-    ],
-  },
-  createdAt: {
-    N: "1714518908277",
-  },
-  id: {
-    S: "3fbc9697-ee49-5368-953b-284393927c99",
-  },
-  lang: {
-    S: "es",
-  },
-};
-
-const sampleResponseError = {
-  summary: {
-    S: 'The Chinese character "一" has several translations in English, depending on the context:\n\n## 1. One\n\nThe most common translation of "一" is "one" when referring to the number 1. For example:\n这是一本书。 - This is one book.\n\n## 2. Single, Whole\n\n"一" can also mean "single" or "whole" when used in phrases like "一条龙" (wholeheartedly) or "一体" (integrated).\n\n## 3. First, Beginning\n\nIn certain contexts, "一" can mean "first" or "beginning". For example:\n一月 - January (the first month of the year)\n一切 - Everything (the beginning of all things)\n\n## 4. Same\n\nWhen used before a noun or pronoun, "一" can indicate "same" or "identical". For example:\n我们是一家人。 - We are one family (We are a family).\n\nSo in summary, the English translations of the Chinese character "一" can include "one", "single/whole", "first/beginning", and "same", depending on the specific context in which it is used.',
-  },
-  sentenceId: {
-    S: "一",
-  },
-  creator: {
-    S: "learnuidev@gmail.com",
-  },
-  // Step 1: Error comes from this data
-  meanings: {
-    L: [
-      {
-        M: {
-          explanation: {
-            S: "Content refers to the substance or material within a written work, website, or other media. It can include articles, images, videos, and other forms of information.",
-          },
-          meaning: {
-            S: "content",
-          },
-          // Step 2: Error comes from this specifc data
-          use_cases: {
-            L: [],
-          },
-        },
-      },
-    ],
-  },
-  createdAt: {
-    N: "1713902781655",
-  },
-  id: {
-    S: "3c5dc9c0-92f8-5ee0-b36e-b9083ca44ecc",
-  },
-  lang: {
-    S: "zh",
-  },
-};
-
-console.log(transformItem(sampleResponseError));

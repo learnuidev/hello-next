102ef82a1ba0c6d998a2fbc501535e17fdd56379
learnuidev <learnuidev@gmail.com>
2024-06-12 19:40:05 -0400

    wip

--- langs/alphabets-dict.ts
+++ langs/alphabets-dict.ts
@@ -2,6 +2,7 @@ import { arabicAlphabets } from "./arabic/arabic-alphabets";
 import { koreanAlphabets } from "./korean/korean-alphabets";
 import { persianAlphabets } from "./persian/persian-alphabets";
 import { romanianAlphabets } from "./romanian/romanian-alphabets";
+import { urduAlphabets } from "./urdu/urdu-alphabets";
 
 export const alphabetsDict = {
   ar: arabicAlphabets,
@@ -9,4 +10,5 @@ export const alphabetsDict = {
 
   ko: koreanAlphabets,
   ro: romanianAlphabets,
+  ur: urduAlphabets,
 } as any;

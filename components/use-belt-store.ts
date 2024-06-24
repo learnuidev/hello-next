009c6632186eba5510308bd5f946bed4f493986b
learnuidev <learnuidev@gmail.com>
2024-06-24 19:06:09 -0400

    wip

--- components/use-belt-store.ts
+++ components/use-belt-store.ts
@@ -3,6 +3,6 @@ import { create } from "zustand";
 import { belts } from "@/app/nmm/utils";
 
 export const useBeltStore = create((set: any, get: any) => ({
-  selectedBelt: belts?.[6],
+  selectedBelt: belts[0],
   setSelectedBelt: (mode: any) => set({ selectedBelt: mode }),
 }));

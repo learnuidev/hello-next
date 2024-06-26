89545018e43cb9dc8f2deb23384272b8a8f9e986
learnuidev <learnuidev@gmail.com>
2024-06-26 10:28:07 -0400

    remove old code; set floating-bar visibility to false by default

--- components/floating-navbar.tsx
+++ components/floating-navbar.tsx
@@ -128,7 +128,7 @@ const FloatingNavbarComp = () => {
 };
 export const FloatingNavbar = () => {
   const { toast } = useToast();
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   const routeName = usePathname();
 
@@ -161,116 +161,6 @@ export const FloatingNavbar = () => {
           )}
         >
           <FloatingNavbarComp />
-        </div>
-      </div>
-    </div>
-  );
-
-  return (
-    <div
-      onMouseEnter={() => {
-        setShow(true);
-      }}
-      onMouseLeave={() => {
-        setShow(false);
-      }}
-      className={cn("flex w-full fixed z-50 bottom-4")}
-    >
-      <div
-        className={cn(
-          "transition",
-          show ? "visible" : "invisible",
-          "flex items-center w-full justify-center"
-        )}
-      >
-        <div className="px-8  py-2 bg-black no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
-          <div className="space-x-8 flex justify-center items-center w-full">
-            <Link
-              href="/convos"
-              className={`transition ${
-                routeName?.includes("/convos")
-                  ? "text-gray-800 dark:text-gray-300"
-                  : "text-gray-200 dark:text-gray-500"
-              } hover:text-white transition text-xl`}
-            >
-              {routeName?.includes("/convos") ? (
-                <Icons.contentSolid className="hover:text-white transition" />
-              ) : (
-                <Icons.content className="hover:text-white transition" />
-              )}
-            </Link>
-
-            {reviewList?.length > 1 ? (
-              <Link
-                href="/review"
-                className={cn(
-                  routeName?.includes("/review")
-                    ? "text-gray-800 dark:text-gray-300"
-                    : "text-gray-200 dark:text-gray-500",
-                  "transition text-xl "
-                )}
-              >
-                <FontAwesomeIcon
-                  icon={faPlayCircle}
-                  className="hover:text-white transition"
-                />
-              </Link>
-            ) : null}
-
-            <Link
-              href="/timeline"
-              className={`transition ${
-                routeName === "/timeline"
-                  ? "text-gray-800 dark:text-gray-300"
-                  : "text-gray-200 dark:text-gray-500"
-              } hover:text-gray-700 transition text-xl`}
-            >
-              <Icons.verticalStack className="hover:text-white transition" />
-            </Link>
-
-            <Link
-              href="/insights"
-              className={`transition ${
-                routeName === "/insights"
-                  ? "text-gray-800 dark:text-gray-300"
-                  : "text-gray-200 dark:text-gray-500"
-              } hover:text-gray-700 transition text-xl`}
-            >
-              <FontAwesomeIcon
-                icon={faChartColumn}
-                className="hover:text-white transition"
-              />
-            </Link>
-
-            {(!lang || lang === "zh") && (
-              <Link
-                href="/pinyin"
-                className={`transition ${
-                  routeName === "/pinyin"
-                    ? "text-gray-800 dark:text-gray-300"
-                    : "text-gray-200 dark:text-gray-500"
-                } hover:text-gray-700 transition text-xl hidden md:block`}
-              >
-                <FontAwesomeIcon
-                  icon={faTableTree}
-                  className="hover:text-white transition"
-                />
-              </Link>
-            )}
-
-            <Link
-              href="/nmm"
-              className={`transition ${
-                routeName === "/nmm"
-                  ? "text-gray-800 dark:text-gray-300"
-                  : "text-gray-200 dark:text-gray-500"
-              } hover:text-gray-700 transition text-xl`}
-            >
-              <NomadIcon className="hover:text-white transition" />
-            </Link>
-          </div>
-
-          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
         </div>
       </div>
     </div>

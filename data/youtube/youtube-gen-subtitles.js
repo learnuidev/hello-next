ac12db4adf8d0d210e932aadb608459595672d86
learnuidev <learnuidev@gmail.com>
2024-07-04 13:46:02 -0400

    improve subtitle

--- data/youtube/youtube-gen-subtitles.js
+++ data/youtube/youtube-gen-subtitles.js
@@ -3,7 +3,9 @@ let res = [];
 
 // 2. Function to Update the res
 const updateRes = () => {
-  const item = document.querySelector(".ytp-caption-segment")?.innerText;
+  const item = [...document.querySelectorAll(".ytp-caption-segment")]
+    .map((x) => x.innerText)
+    .join(" ");
 
   res.push({
     text: item,
@@ -14,18 +16,20 @@ setInterval(updateRes, 5);
 
 // 3. Function to Format Res
 const formatRes = (res, lang = "zh") => {
-  return [...new Set(res.filter((x) => x.text).map((x) => x.text))].map((x) => {
-    const items = res.filter((item) => item.text === x);
-    const times = items.map((item) => item.time);
-    const start = Math.min(...times);
-    const end = Math.max(...times);
-    return {
-      input: x,
-      roman: "",
-      lang,
-      en: "",
-      start,
-      end,
-    };
-  });
+  return [...new Set(res.filter((x) => x.text).map((x) => x.text))]
+    .map((x) => {
+      const items = res.filter((item) => item.text === x);
+      const times = items.map((item) => item.time);
+      const start = Math.min(...times);
+      const end = Math.max(...times);
+      return {
+        input: x,
+        roman: "",
+        lang,
+        en: "",
+        start,
+        end,
+      };
+    })
+    .sort((a, b) => a.end - b.end);
 };

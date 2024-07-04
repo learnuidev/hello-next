96fd58f9a417b8c62fcd7a9ea9ca9200015e30fa
learnuidev <learnuidev@gmail.com>
2024-07-04 16:18:44 -0400

    wip

--- components/_select-character/character-sentences.tsx
+++ components/_select-character/character-sentences.tsx
@@ -21,7 +21,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useContentViewStore } from "./character-store";
+import { useContentViewStore } from "./use-content-view-store";
 
 const ContentSentences = ({
   characterId,

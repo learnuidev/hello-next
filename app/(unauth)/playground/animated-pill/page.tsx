de664d81486ce9a4b7d51821091b927b332e02f8
learnuidev <learnuidev@gmail.com>
2024-07-18 12:50:23 -0400

    wip

--- app/(unauth)/playground/animated-pill/page.tsx
+++ app/(unauth)/playground/animated-pill/page.tsx
@@ -9,21 +9,27 @@ export default function Home() {
 
   return (
     <div className="grid h-screen place-content-center bg-neutral-100">
-      <ul className="relative mx-auto flex w-fit rounded-full border-2 border-black bg-white p-1">
+      <div className="relative mx-auto flex w-fit h-10 items-center rounded-full border-2 border-black bg-white p-1">
         <Tab>Home</Tab>
         <Tab>Pricing</Tab>
         <Tab>Features</Tab>
         <Tab>Docs</Tab>
         <Tab>Blog</Tab>
-      </ul>
+
+        <Cursor />
+      </div>
     </div>
   );
 }
 
 function Tab({ children }: { children: React.ReactNode }) {
   return (
-    <li className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base">
+    <li className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 mx-4">
       {children}
     </li>
   );
+}
+
+function Cursor() {
+  return <div className="absolute z-0 h-8 w-24 rounded-full bg-black" />;
 }

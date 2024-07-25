c4dae1084448b9dcc395805951ba7722d549c8a3
learnuidev <learnuidev@gmail.com>
2024-07-25 15:40:28 -0400

    remove figlet

--- dragonfly/list-directory-names.js
+++ dragonfly/list-directory-names.js
@@ -1,26 +1,26 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-// async function listDirectoryNames(name, options = { directoriesOnly: true }) {
-//   const filterDirectoriesOnly = (pages) => {
-//     return pages?.filter((pageName) => {
-//       if (options.directoriesOnly) {
-//         return !pageName.includes(".");
-//       }
+async function listDirectoryNames(name, options = { directoriesOnly: true }) {
+  const filterDirectoriesOnly = (pages) => {
+    return pages?.filter((pageName) => {
+      if (options.directoriesOnly) {
+        return !pageName.includes(".");
+      }
 
-//       return true;
-//     });
-//   };
-//   if (name) {
-//     const pathName = path.resolve(`../hello-next/app/${name}`);
-//     const children = await fs.readdir(pathName);
-//     return filterDirectoriesOnly(children);
-//   }
+      return true;
+    });
+  };
+  if (name) {
+    const pathName = path.resolve(`../app/${name}`);
+    const children = await fs.readdir(pathName);
+    return filterDirectoriesOnly(children);
+  }
 
-//   const pathName = path.resolve(`../hello-next/app`);
-//   const children = await fs.readdir(pathName);
-//   return filterDirectoriesOnly(children);
-// }
+  const pathName = path.resolve(`./app`);
+  const children = await fs.readdir(pathName);
+  return filterDirectoriesOnly(children);
+}
 
 // module.exports.listDirectoryNames = listDirectoryNames;
 

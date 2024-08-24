1fd9ce4468573a45e9b632eefe828d5eaad6df30
learnuidev <learnuidev@gmail.com>
2024-08-24 09:42:45 -0400

    py-2 to py-4

--- app/(auth)/convos/_play-v2/play-v2.tsx
+++ app/(auth)/convos/_play-v2/play-v2.tsx
@@ -166,7 +166,7 @@ export const PlayV2 = ({ contentId }: { contentId: string }) => {
                       <div key={JSON.stringify(val)}>
                         <div className="">
                           <div className="text-2xl gap-4">
-                            <div className="py-2">
+                            <div className="py-4">
                               {transcriptions?.map(
                                 (transcription: Transcription) => {
                                   return (
@@ -212,7 +212,7 @@ export const PlayV2 = ({ contentId }: { contentId: string }) => {
               <div>
                 <div className="">
                   <div className="mb-12 text-2xl gap-4">
-                    <div className="py-2">
+                    <div className="py-4">
                       {content?.transcriptions?.map(
                         (transcription: Transcription) => {
                           return (

efb699e54315bd92e2b7de90fe2e3b13627895c6
learnuidev <learnuidev@gmail.com>
2024-10-26 20:56:12 -0400

    hsk-spec added basic spec

--- app/review/hsk-review-mode/hsk-exam-mode/hsk-exam-spec.ts
+++ app/review/hsk-review-mode/hsk-exam-mode/hsk-exam-spec.ts
@@ -1,7 +1,35 @@
-interface IHskQuestion {}
+interface IHSKQuestion {}
 
-interface IHskExam {
+interface IHSKSectionPart {
   title: string;
-  hskLevel: number;
-  questions: IHskQuestion[];
+  questions: IHSKQuestion[];
 }
+
+interface IHSKExamSection {
+  title: string;
+  parts: IHSKSectionPart[];
+}
+
+interface IHSKExam {
+  title: string;
+  duration: number;
+  hskLevel: number;
+  sections: IHSKExamSection[];
+}
+
+const hskSectionExample: IHSKExamSection = {
+  title: "一、听 力",
+  parts: [
+    {
+      title: "第 一 部 分",
+      questions: [],
+    },
+  ],
+};
+
+const hskExamExample: IHSKExam = {
+  title: "HSK 1 Sample Exam",
+  duration: 45,
+  hskLevel: 1,
+  sections: [hskSectionExample],
+};

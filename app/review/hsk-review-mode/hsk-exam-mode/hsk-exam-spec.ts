b6f7d428f2599e529a28df1e6475e610a1d003b5
learnuidev <learnuidev@gmail.com>
2024-10-26 21:36:29 -0400

    expanded spec

--- app/review/hsk-review-mode/hsk-exam-mode/hsk-exam-spec.ts
+++ app/review/hsk-review-mode/hsk-exam-mode/hsk-exam-spec.ts
@@ -2,11 +2,13 @@ interface IHSKQuestion {}
 
 interface IHSKSectionPart {
   title: string;
-  questions: IHSKQuestion[];
+  question?: string;
+  questions?: IHSKQuestion[];
 }
 
 interface IHSKExamSection {
   title: string;
+  type: string;
   parts: IHSKSectionPart[];
 }
 
@@ -19,6 +21,7 @@ interface IHSKExam {
 
 const hskSectionExample: IHSKExamSection = {
   title: "一、听 力",
+  type: "listening",
   parts: [
     {
       title: "第 一 部 分",
@@ -32,4 +35,65 @@ const hskExamExample: IHSKExam = {
   duration: 45,
   hskLevel: 1,
   sections: [hskSectionExample],
+};
+
+const hsk2ExamExample: IHSKExam = {
+  title: "HSK 2 Sample Exam",
+  duration: 55,
+  hskLevel: 2,
+  sections: [
+    {
+      title: "一、听 力",
+      type: "listening",
+      parts: [
+        {
+          title: "第 一 部 分",
+          questions: [
+            {
+              title: "我们家有三的人",
+              imageUrl: "",
+              type: "true-false",
+              answerText: "我们家有三的人",
+              answer: false,
+            },
+            {
+              title: "我们家有三的人",
+              imageUrl: "",
+              type: "true-false",
+              answer: false,
+              answerText: "这是一个自行车",
+            },
+            {
+              title: "我每天坐公共汽车去上班",
+              type: "true-false",
+              imageUrl: "",
+              answer: false,
+              answerText: "这是一个红番茄",
+            },
+          ],
+        },
+        {
+          title: "第 二 部 分",
+          question: "Match the Chinese phrases with their English meanings:",
+          questions: [
+            {
+              title: "我们家有三的人",
+              type: "true-false",
+              answer: "我们家有三的人",
+            },
+            {
+              title: "我们家有三的人",
+              type: "true-false",
+              answer: "这是一个自行车",
+            },
+            {
+              title: "我每天坐公共汽车去上班",
+              type: "true-false",
+              answer: "这是一个红番茄",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };

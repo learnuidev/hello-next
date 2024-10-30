1afe69632f9657701c8e9d44f35d81eeb8fb3284
learnuidev <learnuidev@gmail.com>
2024-10-30 10:34:20 -0400

    remove 1d

--- app/review/review-mode.tsx
+++ app/review/review-mode.tsx
@@ -328,7 +328,7 @@ export function ReviewMode(props: any) {
         ) : showCorrectOptions ? (
           <>
             {[
-              { title: "1d", value: "1d" },
+              // { title: "1d", value: "1d" },
               { title: "3d", value: "3d" },
               { title: "7d", value: "7d" },
               { title: "1m", value: "30d" },

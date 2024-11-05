708edfdc1f69ec81c0e3df7f4c9c1a045a085ecf
learnuidev <learnuidev@gmail.com>
2024-11-05 12:02:20 -0500

    remove bug

--- components/use-belt-store.ts
+++
@@ -1,7 +0,0 @@
-import { belts } from "@/app/nmm/utils";
-import { create } from "zustand";
-
-export const useBeltStore = create((set: any, get: any) => ({
-  selectedBelt: belts[0],
-  setSelectedBelt: (mode: any) => set({ selectedBelt: mode }),
-}));

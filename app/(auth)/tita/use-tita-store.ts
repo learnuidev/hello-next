6c325dad6dba5481033b6580f4a283a773ed2ffd
learnuidev <learnuidev@gmail.com>
2024-11-06 17:16:39 -0500

    added default voice

--- app/(auth)/tita/use-tita-store.ts
+++ app/(auth)/tita/use-tita-store.ts
@@ -3,7 +3,7 @@ import { create } from "zustand";
 export const useTitaStore = create((set: any, get: any) => ({
   lang: "cmn-CN",
   setLang: (mode: any) => set({ lang: mode }),
-  voice: "",
+  voice: "bo",
   setVoice: (char: any) => set({ voice: char }),
   resetVoice: () => set({ voice: null }),
 }));

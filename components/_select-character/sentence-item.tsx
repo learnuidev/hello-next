0af99b1964605138a38d99e355deb301a2e895b5
learnuidev <learnuidev@gmail.com>
2024-11-11 21:05:07 -0500

    fix sentence item

--- components/_select-character/sentence-item.tsx
+++ components/_select-character/sentence-item.tsx
@@ -47,67 +47,64 @@ export const SentenceItem = (props: any) => {
   const Links = () => {
     const hanziOrInput = encodeURIComponent(unEncoded);
     return (
-      <div className="flex justify-between items-center w-full mt-2">
+      <div className="flex gap-2 justify-end items-end w-full pr-2 mt-2 sm:mt-0">
+        {/* {currentPhrase?.audio ? ( */}
+
+        {/* ) : null} */}
         <AudioComponent currentPhrase={currentPhrase} />
 
-        <div className="flex gap-2 justify-end items-end w-full pr-2">
-          {/* {currentPhrase?.audio ? ( */}
+        <Link
+          onClick={() => {
+            if (canTrack) {
+              addHistoryMutation.mutate({
+                lang: lang,
+                // characterId,
+                hanzi: unEncoded,
+                trackingCharacters: containsTrackableCharacters?.map(
+                  (item) => item?.hanzi
+                ),
+                eventType: "CONTENT_VIEWED",
+              } as any);
+            }
+          }}
+          // href={`/nmm/${encodeURIComponent(currentPhrase?.hanzi)}`}
 
-          {/* ) : null} */}
+          href={`/nmm/${hanziOrInput}${lang || selectedComp?.lang ? `?lang=${lang || selectedComp?.lang}` : ``}`}
+          className={`text-sm bg-white dark:bg-black p-2 w-8 h-8 ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`}
+        >
+          <Icons.magnifyingGlass />
+        </Link>
 
-          <Link
-            onClick={() => {
-              if (canTrack) {
-                addHistoryMutation.mutate({
-                  lang: lang,
-                  // characterId,
-                  hanzi: unEncoded,
-                  trackingCharacters: containsTrackableCharacters?.map(
-                    (item) => item?.hanzi
-                  ),
-                  eventType: "CONTENT_VIEWED",
-                } as any);
-              }
-            }}
-            // href={`/nmm/${encodeURIComponent(currentPhrase?.hanzi)}`}
+        <GoogleLink hanzi={unEncoded} className={"h-8 w-8"} />
 
-            href={`/nmm/${hanziOrInput}${lang || selectedComp?.lang ? `?lang=${lang || selectedComp?.lang}` : ``}`}
+        {isSuperAdmin && currentPhrase?.id && (
+          <button
+            disabled={
+              deleteSentenceMutation?.isLoading ||
+              deleteSentenceMutation.isSuccess
+            }
             className={`text-sm bg-white dark:bg-black p-2 w-8 h-8 ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`}
+            onDoubleClick={() => {
+              deleteSentenceMutation?.mutateAsync({
+                id: currentPhrase?.id,
+                component: componentId,
+              });
+            }}
           >
-            <Icons.magnifyingGlass />
-          </Link>
-
-          <GoogleLink hanzi={unEncoded} className={"h-8 w-8"} />
-
-          {isSuperAdmin && currentPhrase?.id && (
-            <button
-              disabled={
-                deleteSentenceMutation?.isLoading ||
-                deleteSentenceMutation.isSuccess
-              }
-              className={`text-sm bg-white dark:bg-black p-2 w-8 h-8 ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`}
-              onDoubleClick={() => {
-                deleteSentenceMutation?.mutateAsync({
-                  id: currentPhrase?.id,
-                  component: componentId,
-                });
-              }}
-            >
-              {deleteSentenceMutation?.isLoading ? (
-                <Icons.spinner spinPulse />
-              ) : (
-                <Icons.trash />
-              )}
-            </button>
-          )}
-        </div>
+            {deleteSentenceMutation?.isLoading ? (
+              <Icons.spinner spinPulse />
+            ) : (
+              <Icons.trash />
+            )}
+          </button>
+        )}
       </div>
     );
   };
 
   return (
-    <div className="flex flex-col items-start justify-between py-4 w-full">
-      <div role="button" className="flex flex-col">
+    <div className="flex flex-col sm:flex-row items-center justify-between py-4 w-full">
+      <div role="button" className="flex flex-col w-full">
         {" "}
         <Link
           target="_blank"

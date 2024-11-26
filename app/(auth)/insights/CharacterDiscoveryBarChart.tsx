0ad6a1044bd2248499f692216e44e79cb596217e
learnuidev <learnuidev@gmail.com>
2024-11-26 12:09:03 -0500

    fix build failure

--- app/(auth)/insights/CharacterDiscoveryBarChart.tsx
+++ app/(auth)/insights/CharacterDiscoveryBarChart.tsx
@@ -1,12 +1,9 @@
 import { WeeklyBarChart } from "./WeeklyBarChart";
-import { useInsightsState } from "./use-insights-state";
 
 export const CharacterDiscoveryBarChart = () => {
-  const toDate = useInsightsState((state) => state.toDate);
-
   return (
     <div className="w-full hidden md:block">
-      <WeeklyBarChart toDate={toDate} />
+      <WeeklyBarChart />
     </div>
   );
 };

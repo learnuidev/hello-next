cf0eb4bb9a32fed0a8d5d79b344389d998941dd6
learnuidev <learnuidev@gmail.com>
2024-11-28 14:54:59 -0500

    wip

--- components/floating-character-navbar.tsx
+++ components/floating-character-navbar.tsx
@@ -254,6 +254,20 @@ export const FloatingCharacterNavbar = (props: SelectedCharacterProps) => {
             )}
 
             <SelectedCharacterContentsButton {...props} />
+
+            {characterId?.length === 1 && (
+              <button
+                className={cn(
+                  "text-xl transition",
+                  view === "pinyin" ? "text-white" : "text-gray-400"
+                )}
+                onClick={() => {
+                  setView("pinyin");
+                }}
+              >
+                <Icons.pinyinChart />
+              </button>
+            )}
           </div>
 
           <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />

86ec49be85c5c0ad97568a136753ed28a9a4e147
learnuidev <learnuidev@gmail.com>
2024-11-28 14:49:38 -0500

    wip

--- components/floating-character-navbar.tsx
+++ components/floating-character-navbar.tsx
@@ -253,7 +253,7 @@ export const FloatingCharacterNavbar = (props: SelectedCharacterProps) => {
               <SelectedCharacterStoryButton {...props} />
             )}
 
-            {learnedChar && <SelectedCharacterContentsButton {...props} />}
+            <SelectedCharacterContentsButton {...props} />
           </div>
 
           <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />

1702a95241e7a90ad8a971cce83c7202d71731fe
learnuidev <learnuidev@gmail.com>
2024-11-28 22:55:51 -0500

    wip

--- app/profile/utils/format-percentage.ts
+++ app/profile/utils/format-percentage.ts
@@ -2,6 +2,6 @@ export function formatPercentage(number: number) {
   return Intl.NumberFormat("en-GB", {
     style: "percent",
     minimumFractionDigits: 1,
-    maximumFractionDigits: 2,
+    maximumFractionDigits: 1,
   }).format(number);
 }

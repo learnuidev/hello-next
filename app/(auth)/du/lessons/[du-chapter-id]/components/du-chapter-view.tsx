ac4200726918ba3d00b13a963bb68b499ef910e2
learnuidev <learnuidev@gmail.com>
2024-11-30 09:07:50 -0500

    wip

--- app/(auth)/du/lessons/[du-chapter-id]/components/du-chapter-view.tsx
+++ app/(auth)/du/lessons/[du-chapter-id]/components/du-chapter-view.tsx
@@ -20,9 +20,11 @@ export const DuLessonView = () => {
     cookie: `oHAI2NVPZgiAz%2F%2FR4YbLaUgYCpCxgIuwHUSADEqYngzUXQeBlLaUBAvozrtvHy8vbDy9v0dNJcHOa5FMPKwC6xmxLn3PtZRz6OHGzY7On0eiGL2t0rsJ2nDeYiiwyqzJdLRj18Xf0nqrDtiWXxQKh%2BHquYx49VE8WAtHH3GR1q3dd7idL2PFiYU384VoqqrJ8PtaVtXnkQg2i1W%2BQJ9QdbvPCSq%2BhdqhTI21teAtRZUSeIE9FmDBFPsWJiL7Q%2FAXvOeC4Lj9PYKF2lWZ3IwVnluL0UYuhVujxlhWwOTnylx1tcr9Q%2BTEvYNiziVaeyJbSO7DNLsXrkM2Ui53D1NppsTaLXv%2Bi8We71NmnvCz5HseHB%2BEPLc%2BEMYRpO02siEFKW18g7e1%2B1YyAJ0L7uWiYSHsfsj2jdm13TW1Y6XIC3JnHebFxBSGWozHZ3FUrtR%2BgpNtj37N5fIf8My%2B8DASmen8kacwr8VJURBZTP3TDdaLBGi7YAVbLGIXhAySZhK3L%2BQSSXWUxgVf6cM0ZJcAYVTZpgsDSnKnvFhD0PKGVoyL6GDF%2BAG5oH2Dmdu%2BDEEYV%2FPy7u01JODCvQxMIVvFJJ%2Bfq5go0YMU%2BA0%3D--5wP9B4t69qnaAiEB--CcfIMirI1Ix1oPBxDhXIQQ%3D%3D`,
   });
 
-  // const lesson = data?.lessons?.[0];
-
   const course = data?.course;
+
+  if (!course) {
+    return null;
+  }
 
   return (
     <div className="">
@@ -44,7 +46,7 @@ export const DuLessonView = () => {
         <h1 className="text-2xl font-bold">{course?.title}</h1>
       </div> */}
 
-      <div className="mt-12 max-w-4xl m-auto">
+      <div className="mt-12 max-w-4xl m-auto mb-32">
         {data?.subtitles?.words?.map((subtitle) => {
           // if (JSON.stringify(subtitle)?.includes("\n")) {
           //   return <h1 key={JSON.stringify(subtitle)}>YOOO</h1>;

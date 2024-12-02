7eeede3121d8f40d2b14769a3a1903c76abe1685
learnuidev <learnuidev@gmail.com>
2024-12-02 10:51:33 -0500

    fix bug

--- app/nmm/nmm-utils/filter-non-hanyu.ts
+++ app/nmm/nmm-utils/filter-non-hanyu.ts
@@ -1,3 +1,31 @@
+const characterMap = {
+  ā: "a",
+  á: "a",
+  ǎ: "a",
+  à: "a",
+  ē: "e",
+  é: "e",
+  ě: "e",
+  è: "e",
+  ī: "i",
+  í: "i",
+  ǐ: "i",
+  ì: "i",
+  ō: "o",
+  ó: "o",
+  ǒ: "o",
+  ò: "o",
+  ū: "u",
+  ú: "u",
+  ǔ: "u",
+  ù: "u",
+  ü: "u",
+  ǚ: "u",
+  ǖ: "u",
+  ǘ: "u",
+  ǜ: "u",
+} as any;
+
 export const filterNonHanYu = (hanzi: string): boolean =>
   ![
     "？",
@@ -116,4 +144,10 @@ export const filterNonHanYu = (hanzi: string): boolean =>
     "—",
     "　",
     "\n",
+    "ì",
+    "ī",
+    "é",
+    ">",
+    "<",
+    ...Object.keys(characterMap),
   ]?.includes(hanzi?.toLowerCase());

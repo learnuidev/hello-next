87ffeeca6f801d5251d09aee8f6e994d99518999
learnuidev <learnuidev@gmail.com>
2024-12-11 09:04:29 -0500

    fixed lottie

--- app/nmm/lottie-loading-animation.tsx
+++ app/nmm/lottie-loading-animation.tsx
@@ -1,9 +1,10 @@
 import Lottie from "lottie-react";
 import groovyWalkAnimation from "./loading_animation.json";
+import { cn } from "@/lib/utils";
 
-export function LottieLoadingAnimation() {
+export function LottieLoadingAnimation({ className }: { className?: string }) {
   return (
-    <div className="content-center my-64">
+    <div className={cn("content-center my-8 sm:my-32", className)}>
       <Lottie
         className="h-60"
         // animationData={rocketAnimation}

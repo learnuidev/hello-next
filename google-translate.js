2236716c29cd1f4fe1a711b15937fc63c6e83b76
learnuidev <learnuidev@gmail.com>
2024-12-19 10:09:01 -0500

    refactor

--- google-translate.js
+++ google-translate.js
@@ -1,13 +1,11 @@
 const { pinyin } = require("pinyin-pro");
 
-const translate = async ({ text, targetLang = "en" }) => {
+const translateTo = async ({ text, targetLang = "en" }) => {
   const url = `https://translate-pa.googleapis.com/v1/translateHtml`;
 
   const xApiKey = `AIzaSyATBXajvzQLTDHEQbcpq0Ihe0vWDHmO520`;
 
   const payload = [[[text], "auto", targetLang], "te_lib"];
-
-  const pinyinVal = pinyin(text);
 
   const resp = await fetch(url, {
     method: "POST",
@@ -18,8 +16,13 @@ const translate = async ({ text, targetLang = "en" }) => {
       // "x-client-data": `CIi2yQEIpbbJAQipncoBCN3bygEIlaHLAQiJo8sBCJz+zAEIhqDNAQjh0M4BGI/OzQE=`,
     },
   });
-
   const data = await resp.json();
+  return data?.[0]?.[0];
+};
+
+const translate = async ({ text, targetLang = "en" }) => {
+  const pinyinVal = pinyin(text);
+  const data = await translateTo({ text, targetLang });
 
   return {
     input: text,
@@ -28,10 +31,11 @@ const translate = async ({ text, targetLang = "en" }) => {
   };
 };
 
-translate({
+translateTo({
   // text: "我是歆姐招的法务部的实习生",
-  text: "你好！我的名字是",
-  targetLang: "en",
+  // text: "你好！我的名字是",
+  text: "Bonjour",
+  targetLang: "zh-CN",
 }).then((data) => {
   console.log("DATA", data);
 });

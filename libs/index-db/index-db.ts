905a9b7db237aba9b2a4ba3933ca680fb05501d8
learnuidev <learnuidev@gmail.com>
2025-01-01 09:50:49 -0500

    remove noise

--- libs/index-db/index-db.ts
+++ libs/index-db/index-db.ts
@@ -5,15 +5,12 @@ import { get, set, del } from "idb-keyval"; // can use anything: IndexedDB, Ioni
 // Custom storage object
 export const indexDBStorage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
-    console.log(name, "has been retrieved");
     return (await get(name)) || null;
   },
   setItem: async (name: string, value: string): Promise<void> => {
-    console.log(name, "with value", value, "has been saved");
     await set(name, value);
   },
   removeItem: async (name: string): Promise<void> => {
-    console.log(name, "has been deleted");
     await del(name);
   },
 };

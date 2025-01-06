ab497995d01b598197173c2c1407c920f0cc47d6
learnuidev <learnuidev@gmail.com>
2025-01-06 17:03:28 -0500

    fix bug

--- app/(auth)/convos/new-content/utils/is-audio.ts
+++ app/(auth)/convos/new-content/utils/is-audio.ts
@@ -1,12 +1,11 @@
 export function isAudio(file: any) {
-  const isTrue =
-    ["video/mp4"]?.includes(file?.type) ||
-    file?.path?.includes("mp3") ||
-    file?.path?.includes("wav");
-
-  if (isTrue) {
-    return isTrue;
+  if (typeof file === "string") {
+    return file?.includes("wav") || file?.includes("mp3");
   }
 
-  return file?.includes("wav") || file?.includes("mp3");
+  return (
+    ["video/mp4"]?.includes(file?.type) ||
+    file?.path?.includes("mp3") ||
+    file?.path?.includes("wav")
+  );
 }
--- app/(auth)/convos/new-content/utils/is-video.ts
+++ app/(auth)/convos/new-content/utils/is-video.ts
@@ -1,12 +1,11 @@
 export function isVideo(file: any) {
-  const isTrue =
-    ["video/mp4"]?.includes(file.type) ||
-    file.path?.includes("mp4") ||
-    file?.path?.includes("m4a");
-
-  if (isTrue) {
-    return isTrue;
+  if (typeof file === "string") {
+    return file?.includes("mp4") || file?.includes("m4a");
   }
 
-  return file?.includes("mp4") || file?.includes("m4a");
+  return (
+    ["video/mp4"]?.includes(file.type) ||
+    file.path?.includes("mp4") ||
+    file?.path?.includes("m4a")
+  );
 }

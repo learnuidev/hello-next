4a943803668616de156f002cd58f6051c86b97d0
learnuidev <learnuidev@gmail.com>
2025-01-20 15:11:43 -0500

    fix bug

--- components/language-selector/use-current-lang-store.ts
+++ components/language-selector/use-current-lang-store.ts
@@ -1,7 +1,7 @@
 import { createIndexDBStore } from "@/libs/index-db/index-db";
 
 export const useCurrentLangStore = createIndexDBStore({
-  name: "mandarino/current-lang",
+  name: "mandarino/current-lang-2",
   handler: (set: any, get: any) => ({
     currentLang: "zh",
     setCurrentLang: (event: any) => set({ currentLang: event }),

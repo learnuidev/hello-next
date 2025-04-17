e63e3cf9a5b63fe3f888d96952cffc80ddf7c760
learnuidev <learnuidev@gmail.com>
2025-04-17 01:55:13 -0400

    added more languages

--- app/next/features/phrase/languages.ts
+++ app/next/features/phrase/languages.ts
@@ -8,6 +8,9 @@ import germanFlag from "./german.webp";
 import russianFlag from "./russian.webp";
 import iranFlag from "./fa.webp";
 import palestineFlag from "./ar.png";
+import nepaliFlag from "./ne.png";
+import indianFlag from "./hi.png";
+import pakistanFlag from "./pk.png";
 
 export const languages = [
   {
@@ -69,5 +72,23 @@ export const languages = [
     id: "ar-AR",
     title: "Arabic",
     src: palestineFlag.src,
+  },
+  {
+    shortId: "ne",
+    id: "ne-NE",
+    title: "Nepali",
+    src: nepaliFlag.src,
+  },
+  {
+    shortId: "hi",
+    id: "hi-HI",
+    title: "Hindi",
+    src: indianFlag.src,
+  },
+  {
+    shortId: "ur",
+    id: "ur-UR",
+    title: "Urdu",
+    src: pakistanFlag.src,
   },
 ];

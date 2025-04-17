47c3c49aaa0b042e2e9f4e3726b66f5d40581c37
learnuidev <learnuidev@gmail.com>
2025-04-17 02:00:20 -0400

    added hindi alphabets

--- langs/alphabets-dict.ts
+++ langs/alphabets-dict.ts
@@ -1,14 +1,16 @@
+import { nepaliConsonants, nepaliVowels } from "@/components/devanagari/data";
 import { arabicAlphabets } from "./arabic/arabic-alphabets";
 import { koreanAlphabets } from "./korean/korean-alphabets";
 import { persianAlphabets } from "./persian/persian-alphabets";
 import { romanianAlphabets } from "./romanian/romanian-alphabets";
 import { russianAlphabets } from "./russian/russian-alphabets";
 import { urduAlphabets } from "./urdu/urdu-alphabets";
+import { hindiAlphabets } from "./hindi/hindi-alphabets";
 
 export const alphabetsDict = {
+  hi: hindiAlphabets,
   ar: arabicAlphabets,
   fa: persianAlphabets,
-
   ko: koreanAlphabets,
   ro: romanianAlphabets,
   ur: urduAlphabets,

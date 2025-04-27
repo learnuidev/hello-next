e78150cba9552c81369ffdd950e91e5e8b80fa09
learnuidev <learnuidev@gmail.com>
2025-04-26 20:54:51 -0400

    fix bug

--- app/(auth)/convos/play-v3/transcriptions-view.tsx
+++ app/(auth)/convos/play-v3/transcriptions-view.tsx
@@ -56,38 +56,39 @@ export const TranscriptionsView = ({
                     textSize?.[3]
                   )}
                 >
-                  {viewPinyin && ["zh", "zh-CN"].includes(subtitle.lang) && (
-                    <Link
-                      onClick={() => {
-                        setIfExists({ ...subtitle, contentId });
-                      }}
-                      href={`/nmm/${encodeURIComponent(subtitle?.input || subtitle.hanzi)}?lang=${subtitle?.lang || "zh"}`}
-                      target="_blank"
-                      className={cn(
-                        subtitle?.pinyin ? "text-gray-500" : "text-black",
-                        "text-sm",
-                        currentTime > subtitle?.start &&
-                          currentTime < subtitle.end
-                          ? "text-white "
-                          : "text-gray-500",
+                  {viewPinyin &&
+                    ["zh", "zh-CN", "ur"].includes(subtitle.lang) && (
+                      <Link
+                        onClick={() => {
+                          setIfExists({ ...subtitle, contentId });
+                        }}
+                        href={`/nmm/${encodeURIComponent(subtitle?.input || subtitle.hanzi)}?lang=${subtitle?.lang || "zh"}`}
+                        target="_blank"
+                        className={cn(
+                          subtitle?.pinyin ? "text-gray-500" : "text-black",
+                          "text-sm",
+                          currentTime > subtitle?.start &&
+                            currentTime < subtitle.end
+                            ? "text-white "
+                            : "text-gray-500",
 
-                        textSize?.[0],
-                        activeSubtitle?.sentence === subtitle?.sentence
-                          ? "text-gray-400"
-                          : "text-gray-600",
-                        currentTime > subtitle?.start &&
-                          currentTime < subtitle.end
-                          ? "dark:text-white text-black"
-                          : "",
-                        currentTime === 0
-                          ? "dark:text-gray-300 text-black"
-                          : "",
-                        "text-start"
-                      )}
-                    >
-                      {subtitle?.roman || subtitle?.pinyin || ""}
-                    </Link>
-                  )}
+                          textSize?.[0],
+                          activeSubtitle?.sentence === subtitle?.sentence
+                            ? "text-gray-400"
+                            : "text-gray-600",
+                          currentTime > subtitle?.start &&
+                            currentTime < subtitle.end
+                            ? "dark:text-white text-black"
+                            : "",
+                          currentTime === 0
+                            ? "dark:text-gray-300 text-black"
+                            : "",
+                          "text-start"
+                        )}
+                      >
+                        {subtitle?.roman || subtitle?.pinyin || ""}
+                      </Link>
+                    )}
 
                   {brightMode ? (
                     <div>
--- components/_select-character/utils/is-non-roman-lang.ts
+++ components/_select-character/utils/is-non-roman-lang.ts
@@ -1,4 +1,14 @@
-const nonRomanLanguages = ["zh", "ru", "ja", "hi", "ne", "ko", "ar", "fa"];
+const nonRomanLanguages = [
+  "zh",
+  "ru",
+  "ja",
+  "hi",
+  "ne",
+  "ko",
+  "ar",
+  "fa",
+  "ur",
+];
 
 export const isNonRomanLang = (lang: string) =>
   nonRomanLanguages.includes(lang);

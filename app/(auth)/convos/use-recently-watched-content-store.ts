eaf70511baf2a25972533c8fb22ebf8d346db29f
learnuidev <learnuidev@gmail.com>
2025-05-11 17:13:39 -0400

    remove old code

--- app/(auth)/convos/use-recently-watched-content-store.ts
+++ app/(auth)/convos/use-recently-watched-content-store.ts
@@ -1,35 +1,13 @@
 import { useGetUserPreferenceQuery } from "@/domain/user/use-get-user-preference-query";
 import { useUpdateUserPrefenceMutation } from "@/domain/user/use-update-user-preference-mutation";
-import { createIndexDBStore } from "@/libs/index-db/index-db";
-
-const useRecentlyWatchedContentStore = createIndexDBStore({
-  name: "recently-watched-content",
-  handler: (set: any, get: any) => ({
-    recentlyWatched: {},
-    setRecentlyWatched: (f: any) =>
-      typeof f === "function"
-        ? set({ recentlyWatched: f(get().recentlyWatched) })
-        : set({
-            recentlyWatched: {
-              ...get().recentlyWatched,
-              [f?.id]: { ...f, watchedAt: Date.now() },
-            },
-          }),
-  }),
-});
 
 export const useRecentlyWatchedContent = () => {
   const { data: userPreferences } = useGetUserPreferenceQuery();
-  // const _recentlyWatched: any = useRecentlyWatchedContentStore(
-  //   (state) => state.recentlyWatched
-  // );
 
   const updateUserPreferenceMutation = useUpdateUserPrefenceMutation();
 
   const _recentlyWatched = userPreferences?.recentlyWatched || {};
-  // const setRecentlyWatched = useRecentlyWatchedContentStore(
-  //   (state) => state.setRecentlyWatched
-  // );
+
   const setRecentlyWatched = (item: any) => {
     updateUserPreferenceMutation?.mutate({
       recentlyWatched: {

3a24303b51a63556d9c96d10e0b097f79d2546e4
learnuidev <learnuidev@gmail.com>
2025-05-23 17:42:35 -0400

    fix total words

--- app/(auth)/clipboard/hooks/use-get-total-words.ts
+++ app/(auth)/clipboard/hooks/use-get-total-words.ts
@@ -4,11 +4,18 @@ import { useMemo } from "react";
 
 import { useClipboardState } from "./use-clipboard-state";
 import { useClipboardWords } from "./use-clipboard-words";
+import { useListDictionaryMeaningsQuery } from "@/app/next/features/html-parser/hooks/use-dictionary-list-meanings";
+import { useGetCurrentLang } from "@/hooks/use-get-current-lang";
 
 export function useGetTotalWords() {
   const { state } = useClipboardState();
 
   const { words } = useClipboardWords();
+
+  const lang = useGetCurrentLang();
+
+  const { data: context, isLoading: isContextLoading } =
+    useListDictionaryMeaningsQuery(state);
 
   const wordsList = Object.entries(words)
     .filter((item) => state?.includes(item?.[0]))
@@ -23,5 +30,5 @@ export function useGetTotalWords() {
 
   const totalWords = uniqueWordsList?.length || 0;
 
-  return totalWords;
+  return lang !== "zh" ? context?.length || 0 : totalWords;
 }

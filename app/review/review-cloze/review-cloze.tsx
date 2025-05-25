f705499b9013313431ae06ec3935dd17ac14d2e4
learnuidev <learnuidev@gmail.com>
2025-05-25 10:32:54 -0400

    fix bug

--- app/review/review-cloze/review-cloze.tsx
+++ app/review/review-cloze/review-cloze.tsx
@@ -91,11 +91,18 @@ export function ReviewCloze({
   const contentSentences = useMemo(
     () =>
       isContent
-        ? content?.transcriptions?.filter((transcription: any) => {
-            return (transcription?.hanzi || transcription?.input)?.includes(
-              currentCharacter
-            );
-          })
+        ? content?.transcriptions
+            ?.filter((transcription: any) => {
+              return (transcription?.hanzi || transcription?.input)?.includes(
+                currentCharacter
+              );
+            })
+            .map((item: any) => {
+              return {
+                ...item,
+                contentId: mode,
+              };
+            })
         : [],
     [isContent, content]
   );

4c009d1440160cf2b40a21f96964df4c76c75c41
learnuidev <learnuidev@gmail.com>
2025-05-26 14:20:13 -0400

    wip

--- app/review/review-cloze-content/review-item-hanzi.tsx
+++ app/review/review-cloze-content/review-item-hanzi.tsx
@@ -2,6 +2,7 @@
 import { CharacterItem } from "@/components/_select-character/character-item";
 import { smartSplit } from "@/components/youtube-page/utils/smart-split";
 import Link from "next/link";
+import { useMemo } from "react";
 
 export const ReviewItemHanzi = ({
   input,
@@ -10,24 +11,25 @@ export const ReviewItemHanzi = ({
   input: string;
   lang: string;
 }) => {
+  const splittedItem = useMemo(() => {
+    return smartSplit({ input, lang });
+  }, [smartSplit, input, lang]);
   return (
     <h1 className="text-center text-3xl">
-      {smartSplit({ input: input, lang })
-        .split("")
-        .map((item: string, idx: number) => {
-          return (
-            <Link
-              href={`/nmm/${item}${lang ? `?lang=${lang}` : ""}`}
-              key={`review-cloze-${idx}-${item}`}
-              target="_blank"
-            >
-              <CharacterItem
-                character={item}
-                className="text-center text-3xl font-light"
-              />
-            </Link>
-          );
-        })}
+      {splittedItem?.map((item: string, idx: number) => {
+        return (
+          <Link
+            href={`/nmm/${item}${lang ? `?lang=${lang}` : ""}`}
+            key={`review-cloze-${idx}-${item}`}
+            target="_blank"
+          >
+            <CharacterItem
+              character={item}
+              className="text-center text-3xl font-light"
+            />
+          </Link>
+        );
+      })}
     </h1>
   );
 };

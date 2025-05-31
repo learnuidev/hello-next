15f9df8eb50ee30420b7d4a1a132f690722f2b52
learnuidev <learnuidev@gmail.com>
2025-05-31 12:11:24 -0400

    remove sentry

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,6 @@ import "regenerator-runtime/runtime";
 import { QueryClientProvider } from "@/libs/react-query";
 import "./globals.css";
 import "./zhongwen.css";
-// import "../libs/sentry/sentry-init";
-import "../instrumentation-client";
-import "../instrumentation";
 
 import { Inter } from "next/font/google";
 

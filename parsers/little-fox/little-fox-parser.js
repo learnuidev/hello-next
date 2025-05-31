850c15553c10064e02043a23a61f928a632a12c6
learnuidev <learnuidev@gmail.com>
2025-05-31 14:15:11 -0400

    wip

--- parsers/little-fox/little-fox-parser.js
+++ parsers/little-fox/little-fox-parser.js
@@ -1,8 +1,8 @@
 const littleFoxParser = () => {
   const extractElements = (child) => {
-    const pinyins = child.children[0].children[0].innerText.split("\n");
-    const hanzis = child.children[1].children[0].innerText.split("\n");
-    const english = child.children[2].children[0].innerText.split("\n");
+    const pinyins = child?.children?.[0].children?.[0].innerText.split("\n");
+    const hanzis = child?.children?.[1].children?.[0].innerText.split("\n");
+    const english = child?.children?.[2].children?.[0].innerText.split("\n");
     return pinyins.map((pinyin, x) => {
       return {
         id: crypto.randomUUID(),
@@ -19,3 +19,5 @@ const littleFoxParser = () => {
     ?.map(extractElements)
     ?.flat();
 };
+
+JSON.stringify(littleFoxParser());

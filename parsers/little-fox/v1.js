ce892aac8082cdc6521ddc3fb8f926134314792b
learnuidev <learnuidev@gmail.com>
2025-05-31 14:01:19 -0400

    fix parser

--- parsers/little-fox/v1.js
+++ parsers/little-fox/v1.js
@@ -1,18 +1,21 @@
-const littleFoxParser = () =>
-  JSON.stringify(
-    document
-      .querySelectorAll(".desc")[1]
-      .children[0].children[0].innerHTML.split("\n")
-      .map((x) =>
-        x.replaceAll("<br>", "")?.trim()?.replaceAll("&nbsp;", "")?.trim()
-      )
-      .filter(Boolean)
-      .map((item) => {
-        return {
-          start: 0,
-          end: 0,
-          input: item,
-          lang: "zh",
-        };
-      })
-  );
+const littleFoxParser = () => {
+  const extractElements = (child) => {
+    const pinyins = child.children[0].children[0].innerText.split("\n");
+    const hanzis = child.children[1].children[0].innerText.split("\n");
+    const english = child.children[2].children[0].innerText.split("\n");
+    return pinyins.map((pinyin, x) => {
+      return {
+        id: crypto.randomUUID(),
+        input: hanzis[x],
+        hanzi: hanzis[x],
+        pinyin: pinyin,
+        en: english[x],
+        lang: "zh",
+      };
+    });
+  };
+
+  return [...document.querySelector(".desc").children]
+    ?.map(extractElements)
+    ?.flat();
+};

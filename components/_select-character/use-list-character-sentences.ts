4297026a917c38ce27d5cf1ff4e4b44e3605c614
learnuidev <learnuidev@gmail.com>
2025-06-09 01:31:42 -0400

    initial Improvement

--- components/_select-character/use-list-character-sentences.ts
+++ components/_select-character/use-list-character-sentences.ts
@@ -1,41 +1,38 @@
-import { useSearchParams } from "next/navigation";
-
-import { useListContentsQuery } from "@/domain/content/content.queries";
-
-import { useGetCurrentLang } from "@/hooks/use-get-current-lang";
-import { useContentViewStore } from "./use-content-view-store";
 import { useListPublishedContentsQuery } from "@/app/(auth)/convos/[content-id]/hooks/use-list-published-contents-query";
+import { useMemo } from "react";
+import { useContentViewStore } from "./use-content-view-store";
 
 export const useListCharacterSentences = (characterId: string) => {
   const { data: contentItems } = useListPublishedContentsQuery({});
 
   const contents = contentItems?.items;
-  const searchParams = useSearchParams();
 
   const view = useContentViewStore((state) => state.view);
-  const setView = useContentViewStore((state) => state.setView);
 
-  const searchParamsLang = useGetCurrentLang();
+  const filteredContents = useMemo(
+    () =>
+      contents?.filter((content: any) => {
+        if (view === "all") {
+          return true;
+        }
 
-  const filteredContents = contents?.filter((content: any) => {
-    if (view === "all") {
-      // if (lang) {
-      //   return content?.lang === lang;
-      // }
+        return content?.title === view;
+      }),
+    [contents, view]
+  );
 
-      return true;
-    }
-
-    return content?.title === view;
-  });
-
-  const allSentences = filteredContents
-    ?.map((content: any) => content?.transcriptions)
-    ?.flat()
-    ?.sort(
-      (a: any, b: any) => JSON.stringify(a)?.length - JSON.stringify(b)?.length
-    )
-    ?.filter((item: any) => JSON.stringify(item)?.includes(characterId));
+  const allSentences = useMemo(
+    () =>
+      filteredContents
+        ?.map((content: any) => content?.transcriptions)
+        ?.flat()
+        ?.sort(
+          (a: any, b: any) =>
+            JSON.stringify(a)?.length - JSON.stringify(b)?.length
+        )
+        ?.filter((item: any) => JSON.stringify(item)?.includes(characterId)),
+    [characterId, filteredContents]
+  );
 
   return allSentences;
 };

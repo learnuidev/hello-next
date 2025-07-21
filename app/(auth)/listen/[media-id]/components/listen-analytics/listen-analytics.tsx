d653e0f5952029010b03e82d5320ead16c811aba
learnuidev <learnuidev@gmail.com>
2025-07-21 15:59:50 -0400

    fix bug

--- app/(auth)/listen/[media-id]/components/listen-analytics/listen-analytics.tsx
+++ app/(auth)/listen/[media-id]/components/listen-analytics/listen-analytics.tsx
@@ -78,18 +78,31 @@ export function ListenAnalytics({ mediaId }: { mediaId: string }) {
       <section className="mt-[72px] h-auto sm:min-h-[800px] rounded-2xl dark:bg-[rgb(21,22,23)] bg-gray-100 gap-4 p-4 justify-start">
         <h3 className="text-center mb-12 text-2xl mt-8">Dictionary</h3>
         <NmmListContainerAll>
-          {context?.map((char) => {
-            if (char?.input?.length > 10 || char?.hanzi?.length > 10) {
-              return null;
-            }
-            return (
-              <HanziLink
-                lang={data?.lang}
-                character={char}
-                key={`${char?.id}`}
-              />
-            );
-          })}
+          {wordsRaw
+            ?.map((word) => {
+              const wordItem = context?.find(
+                (item) => item?.hanzi === word || item?.input === word
+              );
+
+              if (wordItem) {
+                return wordItem;
+              } else {
+                return null;
+              }
+            })
+            .filter(Boolean)
+            ?.map((char) => {
+              if (char?.input?.length > 10 || char?.hanzi?.length > 10) {
+                return null;
+              }
+              return (
+                <HanziLink
+                  lang={data?.lang}
+                  character={char}
+                  key={`${char?.id}`}
+                />
+              );
+            })}
         </NmmListContainerAll>
       </section>
     </main>

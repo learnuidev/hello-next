9f18db8568dd2ed2bc50096a0207fd214870ead8
learnuidev <learnuidev@gmail.com>
2025-07-31 21:57:17 -0400

    wip

--- app/(auth)/convos/convo-insights/hooks/use-list-repeat-history.ts
+++ app/(auth)/convos/convo-insights/hooks/use-list-repeat-history.ts
@@ -6,7 +6,7 @@ import { FancyAreaChart } from "../../../insights/insights-v3/components/fancy-a
 import { useRepeatHistoryStore } from "../../_play/use-repeat-history";
 
 const createdAtFormatter = (item: any) =>
-  parseInt(`${item?.createdAt}`?.slice(0, -1));
+  parseInt(`${item?.createdAt}`?.slice(0, -2));
 
 export const useListRepeatHistory = ({ contentId }: { contentId: string }) => {
   const repeatHistories = useRepeatHistoryStore((state: any) => state.history);

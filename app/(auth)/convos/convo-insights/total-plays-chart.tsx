16ab065f23ef8eccfe127f6e7c8210e7a72cb1c0
learnuidev <learnuidev@gmail.com>
2025-08-03 21:40:03 -0400

    added word cloud

--- app/(auth)/convos/convo-insights/total-plays-chart.tsx
+++ app/(auth)/convos/convo-insights/total-plays-chart.tsx
@@ -39,7 +39,7 @@ export const TotalPlaysChart = ({ contentId }: { contentId: string }) => {
 
       {wordsUnique?.length > 0 && (
         <section className="my-[72px] h-auto rounded-2xl dark:bg-[rgb(21,22,23)] bg-gray-100 gap-4 p-4 justify-start">
-          <h3 className="text-center mb-12 text-2xl mt-8">Words Repeat</h3>
+          <h3 className="text-center mb-12 text-2xl mt-8">Words Cloud</h3>
 
           <div className="flex flex-row flex-wrap items-center gap-4">
             {wordsUnique?.map((word) => {

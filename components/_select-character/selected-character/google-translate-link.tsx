13ccd45de9d02704097571477f9f01157ad5f2ac
learnuidev <learnuidev@gmail.com>
2025-08-04 15:37:10 -0400

    wip

--- components/_select-character/selected-character/google-translate-link.tsx
+++ components/_select-character/selected-character/google-translate-link.tsx
@@ -16,7 +16,10 @@ export const GoogleTranslateLink = ({
       href={`https://translate.google.com/?tl=en&text=${encodeURIComponent(
         hanzi
       )}&op=translate`}
-      className={cn(className)}
+      className={cn(
+        `text-sm bg-white dark:bg-black p-2 w-[36px] h-[36px] ring-1 ${`dark:text-white ring-slate-900/5 dark:ring-gray-800`} shadow-lg rounded-full flex items-center justify-center transition`,
+        className
+      )}
     >
       <Icons.language />
     </Link>

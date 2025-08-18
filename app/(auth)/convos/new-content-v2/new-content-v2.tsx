acdb185eef096e32d374e31d0312faae766d0621
learnuidev <learnuidev@gmail.com>
2025-08-17 22:44:21 -0400

    figure out padding

--- app/(auth)/convos/new-content-v2/new-content-v2.tsx
+++ app/(auth)/convos/new-content-v2/new-content-v2.tsx
@@ -12,28 +12,35 @@ export const NewContentV2 = () => {
   // }
 
   return (
-    <div>
-      <Tabs defaultValue={contentTypesV2.youtube.id}>
-        <TabsList>
+    <div className="px-8 sm:px-32">
+      <h1 className="text-2xl font-bold mt-12 mb-8">New Content</h1>
+      <Tabs defaultValue={contentTypesV2.youtube.id} className="bg-none">
+        <TabsList className="bg-white dark:bg-[rgb(9,10,11)] gap-8 p-0">
           {contentTypesListV2.map((contentType) => {
             return (
-              <TabsTrigger key={contentType.id} value={contentType.id}>
+              <TabsTrigger
+                className="px-0 data-[state=active]:shadow-none"
+                key={contentType.id}
+                value={contentType.id}
+              >
                 {contentType.title}
               </TabsTrigger>
             );
           })}
         </TabsList>
 
-        {contentTypesListV2.map((contentType) => {
-          return (
-            <TabsContent
-              value={contentType.id}
-              key={`tab-body-${contentType.id}`}
-            >
-              <contentType.Component />
-            </TabsContent>
-          );
-        })}
+        <div className="mt-8">
+          {contentTypesListV2.map((contentType) => {
+            return (
+              <TabsContent
+                value={contentType.id}
+                key={`tab-body-${contentType.id}`}
+              >
+                <contentType.Component />
+              </TabsContent>
+            );
+          })}
+        </div>
       </Tabs>
     </div>
   );

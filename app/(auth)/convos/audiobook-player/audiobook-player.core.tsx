4529b5ecaf0edd342f4c786b74872377490b9340
learnuidev <learnuidev@gmail.com>
2025-08-31 01:19:14 -0400

    make it snazzy

--- app/(auth)/convos/audiobook-player/audiobook-player.core.tsx
+++ app/(auth)/convos/audiobook-player/audiobook-player.core.tsx
@@ -4,7 +4,10 @@ import { Slider } from "@/components/ui/slider";
 import { useCurrentTime } from "@/components/youtube-page/use-current-time-store";
 
 import { useListDictionaryMeaningsQuery } from "@/app/next/features/html-parser/hooks/use-dictionary-list-meanings";
-import { isNonRomanLang } from "@/components/_select-character/utils/is-non-roman-lang";
+import {
+  isNonRomanLang,
+  isRomanLang,
+} from "@/components/_select-character/utils/is-non-roman-lang";
 import { ChinglishButton } from "@/components/chinglish-button";
 import { PinyinButton } from "@/components/pinyin-button";
 import { useChinglishState } from "@/components/settings-dialog/use-chinglish-state";
@@ -22,6 +25,8 @@ import { useListDiscoveryQuery } from "@/domain/sentence/use-list-discovery-quer
 import { AnimatedLoadingText } from "@/components/animated-loading-text";
 import { useListSentencesQuery } from "@/domain/sentence/sentence.queries";
 import { MandoContextMenu } from "@/app/review/review-cloze-content/mando-context-menu";
+import { smartSplit } from "@/components/youtube-page/utils/smart-split";
+import { CharacterItem } from "@/components/_select-character/character-item";
 
 interface CurrentTranscriptionProps {
   currentTranscription: ContentTranscription;
@@ -112,13 +117,13 @@ function PinyinView({
         </div>
       ) : (
         <div className="mt-16">
-          {isNonRomanLang(currentTranscription?.lang) ? null : (
+          {isNonRomanLang(currentTranscription?.lang) ? (
             <p>
               {currentTranscription?.lang === "zh"
                 ? currentTranscription?.pinyin
                 : currentTranscription?.roman}
             </p>
-          )}
+          ) : null}
           <p
             className={cn(
               currentTranscription?.lang === "zh"
@@ -126,7 +131,21 @@ function PinyinView({
                 : "text-[16px] sm:text-xl"
             )}
           >
-            {currentTranscription?.input}
+            {smartSplit({
+              input: currentTranscription?.input,
+              lang: currentTranscription?.lang,
+            })?.map((item: any, idx: any) => {
+              return (
+                <span key={`${item}-pinin-view-${idx}`}>
+                  <CharacterItem
+                    character={item}
+                    onClick={() => {
+                      setSelected(item);
+                    }}
+                  />
+                </span>
+              );
+            })}
           </p>
         </div>
       )}
@@ -167,6 +186,7 @@ function MiniDictionary({
   lang: string;
   selected: string;
 }) {
+  const [seeMore, setSeeMore] = useState(false);
   const { data: sentences } = useListSentencesQuery({
     component: selected,
     lang,
@@ -182,7 +202,7 @@ function MiniDictionary({
   const pinyinOrRoman = data?.pinyin || data?.roman;
 
   return (
-    <div className="w-full sm:w-96 bg-gray-50 dark:bg-[rgb(13,14,15)] rounded p-4 sm:p-8 sm:mt-20 mt-0">
+    <div className="w-full sm:w-[600px] bg-gray-50 dark:bg-[rgb(13,14,15)] rounded p-4 sm:p-8 sm:mt-20 mt-0">
       <div className="flex justify-between items-center">
         <h4 className="text-2xl font-bold"> {selected} </h4>
 
@@ -208,32 +228,73 @@ function MiniDictionary({
         </div>
       ) : (
         <>
-          <p className="text-gray-400 font-light">{data?.en}</p>
-
-          <p className="text-sm mt-4 dark:text-gray-300 text-gray-700">
-            {data?.explanation ? `${data?.explanation}` : null}
+          <p className="text-gray-600 dark:text-gray-400 font-light">
+            {data?.en}
           </p>
+
+          {seeMore ? (
+            <p className="text-[16px] mt-4 dark:text-gray-300 text-gray-500">
+              {data?.explanation ? `${data?.explanation}` : null}
+            </p>
+          ) : (
+            <div>
+              <p className="text-[16px] mt-4 dark:text-gray-300 text-gray-500">
+                {data?.explanation
+                  ? `${data?.explanation?.slice(0, 240)}...`
+                  : null}
+              </p>
+            </div>
+          )}
         </>
       )}
 
-      <div className="mt-8">
-        <div className="space-y-4">
-          {sentences?.slice(0, 3)?.map((sentence) => {
-            return (
-              <div key={sentence.id}>
-                {lang === "zh" && isNonRomanLang(lang) && (
-                  <p className="text-gray-600 dark:text-gray-400">
-                    {sentence?.pinyin || sentence?.roman}
+      {seeMore && (
+        <div className="mt-8">
+          <h4 className="font-bold mb-4 uppercase text-rose-400">
+            Example Sentences
+          </h4>
+          <div className="space-y-4">
+            {sentences?.slice(0, 3)?.map((sentence) => {
+              return (
+                <div key={sentence.id}>
+                  {lang === "zh" && isNonRomanLang(lang) && (
+                    <p className="text-gray-600 dark:text-gray-400">
+                      {sentence?.pinyin || sentence?.roman}
+                    </p>
+                  )}
+                  <p
+                    className={
+                      lang === "zh" ? "text-2xl" : "text-[16px] sm:text-xl"
+                    }
+                  >
+                    {sentence?.hanzi || sentence?.input}
                   </p>
-                )}
-                <p className={"text-2xl"}>
-                  {sentence?.hanzi || sentence?.input}
-                </p>
-                <p className="text-gray-500">{sentence?.en}</p>
-              </div>
-            );
-          })}
+                  <p className="text-gray-500">{sentence?.en}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
+      )}
+
+      <div className="mt-8 text-gray-500">
+        {seeMore ? (
+          <button
+            onClick={() => {
+              setSeeMore(false);
+            }}
+          >
+            See Less
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setSeeMore(true);
+            }}
+          >
+            See More
+          </button>
+        )}
       </div>
     </div>
   );
@@ -251,7 +312,20 @@ export const AudiobookPlayerCore = ({ content }: { content: IContent }) => {
 
   const playerRef = useRef<any>(null);
 
-  const transcriptions = content?.transcriptions || [];
+  // TODO: move this at api level
+  const transcriptions = (content?.transcriptions || [])?.map(
+    (item, idx, ctx) => {
+      if (idx === 0) {
+        return {
+          ...item,
+          start: item?.start === 0 ? 0.1 : item?.start,
+          end: ctx?.[idx + 1]?.start,
+        };
+      }
+
+      return item;
+    }
+  );
 
   const seek = (time: number) => {
     playerRef.current.seekTo(time, "seconds");
@@ -328,7 +402,7 @@ export const AudiobookPlayerCore = ({ content }: { content: IContent }) => {
 
   const editMode = useContentEditStore((state) => state.editMode);
 
-  const currentTranscription = content?.transcriptions?.find(
+  const currentTranscription = transcriptions?.find(
     (transcription) =>
       transcription?.start <= currentTime && transcription?.end >= currentTime
   );
@@ -389,7 +463,7 @@ export const AudiobookPlayerCore = ({ content }: { content: IContent }) => {
 
   useEffect(() => {
     if (loop) {
-      const selectedWords = content?.transcriptions?.find(
+      const selectedWords = transcriptions?.find(
         (word) => word?.input === loop
       );
 
@@ -397,7 +471,7 @@ export const AudiobookPlayerCore = ({ content }: { content: IContent }) => {
         debounceSeek(selectedWords?.start);
       }
     }
-  }, [currentTime, content?.transcriptions, loop, debounceSeek]);
+  }, [currentTime, transcriptions, loop, debounceSeek]);
 
   useEffect(() => {
     if (isReady) {

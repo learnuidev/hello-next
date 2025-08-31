24ab4cb590ff1d70d3497ff6f853aaca9b22f604
learnuidev <learnuidev@gmail.com>
2025-08-31 00:28:44 -0400

    added sentences

--- app/(auth)/convos/audiobook-player/audiobook-player.core.tsx
+++ app/(auth)/convos/audiobook-player/audiobook-player.core.tsx
@@ -21,6 +21,7 @@ import { formatTime } from "../_play/utils";
 import { useListDiscoveryQuery } from "@/domain/sentence/use-list-discovery-query";
 import { AnimatedLoadingText } from "@/components/animated-loading-text";
 import { useListSentencesQuery } from "@/domain/sentence/sentence.queries";
+import { MandoContextMenu } from "@/app/review/review-cloze-content/mando-context-menu";
 
 interface CurrentTranscriptionProps {
   currentTranscription: ContentTranscription;
@@ -181,12 +182,9 @@ function MiniDictionary({
   const pinyinOrRoman = data?.pinyin || data?.roman;
 
   return (
-    <div className="w-full sm:w-96 bg-gray-50 dark:bg-[rgb(13,14,15)] rounded p-4 sm:p-8">
+    <div className="w-full sm:w-96 bg-gray-50 dark:bg-[rgb(13,14,15)] rounded p-4 sm:p-8 sm:mt-20 mt-0">
       <div className="flex justify-between items-center">
-        <h4 className="text-2xl font-bold">
-          {" "}
-          {selected} {pinyinOrRoman ? `(${pinyinOrRoman})` : null}{" "}
-        </h4>
+        <h4 className="text-2xl font-bold"> {selected} </h4>
 
         <button
           onClick={() => {
@@ -197,6 +195,10 @@ function MiniDictionary({
         </button>
       </div>
 
+      {isNonRomanLang(lang) && (
+        <p>{pinyinOrRoman ? `${pinyinOrRoman}` : null} </p>
+      )}
+
       {isMeaningDiscoveryLoading ? (
         <div className="my-4">
           <AnimatedLoadingText
@@ -206,12 +208,10 @@ function MiniDictionary({
         </div>
       ) : (
         <>
-          <p className="text-gray-400 font-light text-xl">{data?.en}</p>
+          <p className="text-gray-400 font-light">{data?.en}</p>
 
-          <p className="text-sm mt-4 text-gray-500">
-            {data?.explanation
-              ? `${data?.explanation?.split(".")?.[0]}.`
-              : null}
+          <p className="text-sm mt-4 dark:text-gray-300 text-gray-700">
+            {data?.explanation ? `${data?.explanation}` : null}
           </p>
         </>
       )}
@@ -412,98 +412,100 @@ export const AudiobookPlayerCore = ({ content }: { content: IContent }) => {
   const containsChinglish = !!transcriptions?.[0]?.chinglish;
 
   return (
-    <div className="relative">
-      <div className="flex flex-col sm:flex-row gap-8 sm:px-8 scroll-px-80">
-        {currentTranscription && (
-          <CurrentTranscriptionView
-            containsChinglish={containsChinglish}
-            seekAndPlay={seekAndPlay}
-            currentTranscription={currentTranscription}
-          />
-        )}
-
-        {selected && (
-          <MiniDictionary selected={selected} lang={content?.lang} />
-        )}
-      </div>
-
-      <div className="fixed bottom-2 w-full">
-        <div className="w-full max-w-3xl mx-auto p-4 py-2">
-          <ReactPlayer
-            playbackRate={playbackRate}
-            progressInterval={100}
-            url={content?.audio}
-            onPlay={() => setPlaying(true)}
-            onPause={() => setPlaying(false)}
-            width="100%"
-            height="50px"
-            onReady={(data) => {
-              setIsReady(true);
-              setDuration(data.getDuration());
-            }}
-            playing={false}
-            controls={false}
-            ref={playerRef}
-            onProgress={(value) => {
-              setCurrentTime(value.playedSeconds);
-            }}
-          />
-
-          <div className="flex items-center justify-center sm:gap-8 gap-4">
-            <button
-              className={cn(
-                "text-xl",
-                loop
-                  ? "dark:text-white text-black font-bold"
-                  : "dark:text-gray-600 text-gray-300"
-              )}
-              onClick={() => {
-                setLoop((loop: string) => {
-                  if (loop) {
-                    return null;
-                  }
-
-                  return currentTranscription?.input;
-                });
-              }}
-            >
-              <Icons.loop />
-            </button>
-            <button onClick={seekBefore} className="p-2 rounded-full ">
-              <Icons.rotateLeft className="text-xl" />
-            </button>
-
-            <button onClick={handlePlayPause} className="rounded-full">
-              {playing ? (
-                <Icons.pause className="text-2xl" />
-              ) : (
-                <Icons.play className="text-2xl" />
-              )}
-            </button>
-
-            <button onClick={seekAfter} className="rounded-full ">
-              <Icons.rotateRight className="text-xl" />
-            </button>
-
-            <PinyinButton className="text-2xl" />
-            {containsChinglish && <ChinglishButton className="text-2xl" />}
-          </div>
-
-          <div className="flex items-center gap-4">
-            <span className="text-sm">{formatTime(currentTime)}</span>
-            <Slider
-              min={0}
-              max={duration}
-              step={1}
-              value={[currentTime]}
-              defaultValue={[currentTime]}
-              onValueChange={handleSeekChange}
-              className="w-full"
+    <MandoContextMenu lang={content?.lang || ""}>
+      <div className="relative">
+        <div className="flex flex-col sm:flex-row gap-8 sm:px-8 scroll-px-80">
+          {currentTranscription && (
+            <CurrentTranscriptionView
+              containsChinglish={containsChinglish}
+              seekAndPlay={seekAndPlay}
+              currentTranscription={currentTranscription}
             />
-            <span className="text-sm">{formatTime(duration)}</span>
+          )}
+
+          {currentTranscription && selected && (
+            <MiniDictionary selected={selected} lang={content?.lang} />
+          )}
+        </div>
+
+        <div className="fixed bottom-2 w-full">
+          <div className="w-full max-w-3xl mx-auto p-4 py-2">
+            <ReactPlayer
+              playbackRate={playbackRate}
+              progressInterval={100}
+              url={content?.audio}
+              onPlay={() => setPlaying(true)}
+              onPause={() => setPlaying(false)}
+              width="100%"
+              height="50px"
+              onReady={(data) => {
+                setIsReady(true);
+                setDuration(data.getDuration());
+              }}
+              playing={false}
+              controls={false}
+              ref={playerRef}
+              onProgress={(value) => {
+                setCurrentTime(value.playedSeconds);
+              }}
+            />
+
+            <div className="flex items-center justify-center sm:gap-8 gap-4">
+              <button
+                className={cn(
+                  "text-xl",
+                  loop
+                    ? "dark:text-white text-black font-bold"
+                    : "dark:text-gray-600 text-gray-300"
+                )}
+                onClick={() => {
+                  setLoop((loop: string) => {
+                    if (loop) {
+                      return null;
+                    }
+
+                    return currentTranscription?.input;
+                  });
+                }}
+              >
+                <Icons.loop />
+              </button>
+              <button onClick={seekBefore} className="p-2 rounded-full ">
+                <Icons.rotateLeft className="text-xl" />
+              </button>
+
+              <button onClick={handlePlayPause} className="rounded-full">
+                {playing ? (
+                  <Icons.pause className="text-2xl" />
+                ) : (
+                  <Icons.play className="text-2xl" />
+                )}
+              </button>
+
+              <button onClick={seekAfter} className="rounded-full ">
+                <Icons.rotateRight className="text-xl" />
+              </button>
+
+              <PinyinButton className="text-2xl" />
+              {containsChinglish && <ChinglishButton className="text-2xl" />}
+            </div>
+
+            <div className="flex items-center gap-4">
+              <span className="text-sm">{formatTime(currentTime)}</span>
+              <Slider
+                min={0}
+                max={duration}
+                step={1}
+                value={[currentTime]}
+                defaultValue={[currentTime]}
+                onValueChange={handleSeekChange}
+                className="w-full"
+              />
+              <span className="text-sm">{formatTime(duration)}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </MandoContextMenu>
   );
 };

7bc236993060529f946908e3b634b818d5d768a6
learnuidev <learnuidev@gmail.com>
2025-09-01 21:35:50 -0400

    fix bug

--- app/nmm/nmm-utils/calculate-hover-color.ts
+++ app/nmm/nmm-utils/calculate-hover-color.ts
@@ -1,16 +1,16 @@
 export const calculateHoverColor = (dict: any) => {
   switch (parseInt(dict?.tone)) {
     case 1:
-      return "hover:text-rose-400";
+      return "hover:text-rose-400 dark:hover:text-rose-400";
     case 2:
-      return "hover:text-teal-400";
+      return "hover:text-teal-400 dark:hover:text-teal-400";
     case 3:
-      return "hover:text-purple-400";
+      return "hover:text-purple-400 dark:hover:text-purple-400";
     case 4:
-      return "hover:text-pink-400";
+      return "hover:text-pink-400 dark:hover:text-pink-400";
     case 5:
       return "hover:dark:text-blue-500 hover:text-blue-500";
     default:
-      return "hover:dark:text-white hover:text-black";
+      return "hover:dark:text-yellow-500 hover:text-yellow-500";
   }
 };

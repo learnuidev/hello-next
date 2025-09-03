9a6298e49df474dce0265cb62eee93c219e4e6f8
learnuidev <learnuidev@gmail.com>
2025-09-03 11:22:38 -0400

    make it flexible

--- components/_select-character/character-title.v2.tsx
+++ components/_select-character/character-title.v2.tsx
@@ -33,8 +33,10 @@ import { useYoutubeRefState } from "./use-youtube-ref-state";
 import { getSelectedText } from "@/app/review/review-cloze-content/utils/get-selected-text";
 import { openInNewWindow } from "@/app/review/review-cloze-content/utils/open-in-new-window";
 import { getNmmLink } from "@/libs/utils/get-nmm-link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { isNonRomanLang } from "./utils/is-non-roman-lang";
+import { WithInteractiveTitle } from "./with-interative-title";
+import Link from "next/link";
 
 export const CharacterTitleV2 = (props: any) => {
   const {
@@ -63,7 +65,9 @@ export const CharacterTitleV2 = (props: any) => {
   const pinyins = data?.map((val) => val?.pinyin) || [];
   const englishMeanings = data?.map((val) => val?.en) || [];
 
-  const { speak } = useSpeak(lang);
+  const searchParams = useSearchParams();
+
+  const context = searchParams?.get("context");
 
   const { data: selectedComp } = useGetComponentQuery({
     hanzi: componentId,
@@ -135,66 +139,185 @@ export const CharacterTitleV2 = (props: any) => {
           }}
           className="text-gray-900 dark:text-gray-400  font-light focus-visible:ring-0 focus-visible:ring-transparent w-full"
         />
-      ) : (
-        <div>
-          {isNonRomanLang(lang) && !data && (
-            <p className="text-gray-900 dark:text-gray-400  font-light focus-visible:ring-0 focus-visible:ring-transparent w-full">
-              {meaningDiscovery?.pinyin}{" "}
-            </p>
-          )}
-          {segmentedData?.map((item, idx) => {
-            return (
-              <span
-                className={cn(
-                  "inline-flex flex-col items-center justify-center",
-
-                  ["，", "。"]?.includes(item?.input)
-                    ? ""
-                    : idx === 0
-                      ? "pr-[2px] sm:pr-[4px]"
-                      : "px-[2px] py-[0px] sm:px-[4px]",
-
-                  "leading-none",
-                  "py-[0px]"
-                )}
-                key={`${JSON.stringify(item)}-${idx}-${idx}`}
-              >
-                {isNonRomanLang(lang) && showPinyin && (
-                  <span className="text-sm dark:text-gray-400 text-gray-800 lowercase">
-                    {formatRoman(item)}
-                  </span>
-                )}
-
+      ) : showPinyin ? (
+        <div className="flex justify-between items-center w-full">
+          <div>
+            {isNonRomanLang(lang) && !data && (
+              <p className="text-gray-900 dark:text-gray-400  font-light focus-visible:ring-0 focus-visible:ring-transparent w-full">
+                {meaningDiscovery?.pinyin}{" "}
+              </p>
+            )}
+            {segmentedData?.map((item, idx) => {
+              return (
                 <span
-                  className={"text-[16px] sm:text-xl"}
-                  onClick={() => {
-                    const selectedText = getSelectedText();
+                  className={cn(
+                    "inline-flex flex-col items-center justify-center",
 
-                    if (selectedText && selectedText?.length < 36) {
-                      router.push(getNmmLink({ id: selectedText, lang }));
-                      // setSelected(selectedText);
-                    } else {
-                      router.push(
-                        getNmmLink({ id: item?.hanzi || item?.input, lang })
-                      );
-                      // setSelected(item);
-                    }
-                  }}
+                    ["，", "。"]?.includes(item?.input)
+                      ? ""
+                      : idx === 0
+                        ? "pr-[2px] sm:pr-[4px]"
+                        : "px-[2px] py-[0px] sm:px-[4px]",
+
+                    "leading-none",
+                    "py-[0px]"
+                  )}
+                  key={`${JSON.stringify(item)}-${idx}-${idx}`}
                 >
-                  {smartSplit({
-                    input: item?.hanzi || item?.input,
-                    lang: lang,
-                  })?.map((item: any, idx: any) => {
+                  {isNonRomanLang(lang) && showPinyin && (
+                    <span className="text-sm dark:text-gray-400 text-gray-800 lowercase">
+                      {formatRoman(item)}
+                    </span>
+                  )}
+
+                  <span
+                    onClick={() => {
+                      const selectedText = getSelectedText();
+
+                      if (selectedText && selectedText?.length < 36) {
+                        router.push(getNmmLink({ id: selectedText, lang }));
+                        // setSelected(selectedText);
+                      } else {
+                        router.push(
+                          getNmmLink({ id: item?.hanzi || item?.input, lang })
+                        );
+                        // setSelected(item);
+                      }
+                    }}
+                  >
+                    {smartSplit({
+                      input: item?.hanzi || item?.input,
+                      lang: lang,
+                    })?.map((item: any, idx: any) => {
+                      return (
+                        <span key={`${item}-pinin-view-${idx}`}>
+                          <CharacterItem
+                            className={
+                              selectedCompInput?.length < 8
+                                ? "lg:text-4xl text-4xl"
+                                : "text-2xl"
+                            }
+                            character={item}
+                            onClick={() => {}}
+                          />
+                        </span>
+                      );
+                    })}
+                  </span>
+                </span>
+              );
+            })}
+          </div>
+
+          <div className="text-2xl">
+            {isHsk && typeof isHsk?.hskLevel === "number" ? (
+              <p>HSK {isHsk?.hskLevel} </p>
+            ) : (
+              selectedCompInput?.length < 4 && <StatusIcon.Icon />
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-between items-center w-full">
+          <WithInteractiveTitle
+            customRef={customRef}
+            text={selectedCompInput}
+            lang={lang}
+            className={
+              selectedCompInput?.length < 8
+                ? "lg:text-4xl text-4xl"
+                : "text-2xl"
+            }
+          >
+            <div>
+              {smartSplit({ input: selectedCompInput, lang })?.map(
+                (item: string, idx: number) => {
+                  const startTime =
+                    (duration / (selectedCompInput?.length - 1)) * idx;
+
+                  const endTime =
+                    (duration / (selectedCompInput?.length - 1)) * (idx + 1);
+
+                  if (isPlaying) {
                     return (
-                      <span key={`${item}-pinin-view-${idx}`}>
-                        <CharacterItem character={item} onClick={() => {}} />
+                      <span
+                        className={
+                          selectedCompInput?.length < 4
+                            ? "text-5xl"
+                            : "text-2xl"
+                        }
+                        key={`character-title-${item}-${idx}-${idx}`}
+                      >
+                        <CharacterItem
+                          className={cn(
+                            selectedCompInput?.length < 8
+                              ? "lg:text-4xl text-4xl"
+                              : "text-2xl",
+
+                            currentTime >= startTime && currentTime <= endTime
+                              ? "dark:text-white text-black"
+                              : "text-gray-500"
+                          )}
+                          onClick={() => {
+                            if (duration) {
+                              seekAndPlay(startTime, customRef);
+                            }
+
+                            // setWords({
+                            //   word: character,
+                            //   transcriptionId: item?.id,
+                            //   contentId,
+                            // });
+                          }}
+                          // disableForgotten
+                          character={item}
+                        />
                       </span>
                     );
-                  })}
-                </span>
-              </span>
-            );
-          })}
+                  }
+
+                  return (
+                    <Link
+                      className={
+                        selectedCompInput?.length < 4 ? "text-5xl" : "text-2xl"
+                      }
+                      key={`character-title-${item}-${idx}-${idx}`}
+                      href={`/nmm/${item}?lang=${lang || "zh"}${context ? `&context=${context}` : ""}`}
+                    >
+                      <CharacterItem
+                        className={
+                          selectedCompInput?.length < 8
+                            ? "lg:text-4xl text-4xl"
+                            : "text-2xl"
+                        }
+                        onClick={() => {
+                          if (duration) {
+                            seekAndPlay(startTime, customRef);
+                          }
+
+                          // setWords({
+                          //   word: character,
+                          //   transcriptionId: item?.id,
+                          //   contentId,
+                          // });
+                        }}
+                        // disableForgotten
+                        character={item}
+                      />
+                    </Link>
+                  );
+                }
+              )}
+            </div>
+          </WithInteractiveTitle>
+
+          <div className="text-2xl">
+            {isHsk && typeof isHsk?.hskLevel === "number" ? (
+              <p>HSK {isHsk?.hskLevel} </p>
+            ) : (
+              selectedCompInput?.length < 4 && <StatusIcon.Icon />
+            )}
+          </div>
         </div>
       )}
 

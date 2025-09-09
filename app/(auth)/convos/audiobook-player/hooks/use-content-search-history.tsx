7e22bbea983e199c6ed707dd4d1820a61cc11759
learnuidev <learnuidev@gmail.com>
2025-09-09 13:49:00 -0400

    wip

--- app/(auth)/convos/audiobook-player/hooks/use-content-search-history.tsx
+++ app/(auth)/convos/audiobook-player/hooks/use-content-search-history.tsx
@@ -15,18 +15,19 @@ export const useContentSearchHistory = ({
 
   const addSearchHistory = (props: { input: string } & any) => {
     addHistoryMutation.mutate({
+      ...props,
       input: props.input,
       lang: content?.lang,
       eventType: "SEARCH",
-      [contentId]: {
-        transcriptionId: props?.transcriptionId,
+      context: {
         contentId,
+        transcriptionId: props?.transcriptionId,
       },
     } as any);
   };
 
   const searchHistory = data?.Items?.filter(
-    (item: any) => item?.[contentId]
+    (item: any) => item?.context?.contentId === contentId
   )?.sort((a: any, b: any) => (a?.lastSeen || 0) - (b?.lastSeen || 0));
 
   return {

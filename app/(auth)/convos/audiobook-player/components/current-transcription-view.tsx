9da54599c054587d6efbe8a4083198e9cece2bac
learnuidev <learnuidev@gmail.com>
2025-09-10 14:07:56 -0400

    wip

--- app/(auth)/convos/audiobook-player/components/current-transcription-view.tsx
+++ app/(auth)/convos/audiobook-player/components/current-transcription-view.tsx
@@ -278,7 +278,10 @@ export function PinyinView({
 }: CurrentTranscriptionProps) {
   const { data: _data } = useListDictionaryMeaningsQuery(
     currentTranscription?.input,
-    currentTranscription?.lang
+    currentTranscription?.lang,
+    {
+      words: currentTranscription?.words,
+    }
   );
 
   const { data: _segmentedData } = useSegmentTextQuery({
@@ -286,7 +289,7 @@ export function PinyinView({
     lang: currentTranscription?.lang,
   });
 
-  const data = _data || _segmentedData;
+  const data = _data || currentTranscription?.words || _segmentedData;
 
   const { selected, setSelected } = useSelectedItem();
 

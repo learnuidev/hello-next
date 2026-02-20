9c09d75ec489887890fc4b3c6e40d7c23743ff19
learnuidev <learnuidev@gmail.com>
2026-02-20 17:56:13 -0500

    added learn section

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from "@/components/navbar";
 import { agenda } from "@/data/agenda";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function LearnMandarino() {
   return (
@@ -11,31 +12,32 @@ export default function LearnMandarino() {
         <div className="col-span-2"></div>
         <div className="col-span-8">
           <div className="grid md:grid-cols-5 lg:grid-cols-8 grid-cols-2 gap-8">
-            {agenda?.map((lesson) => {
+            {agenda.map((lesson) => {
               return (
                 <div key={lesson.id} className="col-span-2">
-                  <p className="text-gray-400 font-light text-sm">
-                    {lesson?.title}
+                  <p className="dark:text-gray-400 font-light text-sm">
+                    {lesson.title}
                   </p>
 
                   <div className="mx-4 my-4">
-                    {lesson?.tasks?.map((task) => {
+                    {lesson.tasks.map((task) => {
                       return (
-                        <div
+                        <Link
+                          href={`/learn/${task.id}`}
                           key={task.id}
                           className={cn(
-                            lesson?.id !== "multi-languages"
+                            lesson.id !== "multi-languages"
                               ? "text-white"
-                              : lesson?.id === "multi-languages" &&
-                                  ["Mandarin", "Nepali", "Romanian"]?.includes(
-                                    task?.title
+                              : lesson.id === "multi-languages" &&
+                                  ["Mandarin", "Nepali", "Romanian"].includes(
+                                    task.title
                                   )
-                                ? "text-white"
-                                : "text-gray-800"
+                                ? "dark:text-white text-black"
+                                : "text-gray-500"
                           )}
                         >
-                          <p>{task?.title}</p>
-                        </div>
+                          <p>{task.title}</p>
+                        </Link>
                       );
                     })}
                   </div>

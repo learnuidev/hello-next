2d052ba6ecf976fffe3d92faf258a875c8c872c2
learnuidev <learnuidev@gmail.com>
2026-02-25 21:54:10 -0500

    added parse html mutation

---
+++ domain/content-service/use-parse-html.mutation.ts
@@ -0,0 +1,40 @@
+import { siteConfig } from "@/lib/config";
+import { useMutation } from "@tanstack/react-query";
+import { useCurrentAuthUser } from "../auth/auth.queries";
+import { ContentV2 } from "./content-v2.types";
+
+interface ParseHtmlRequest {
+  websiteUrl: string;
+}
+interface ParseHtmlResponse {
+  html: string;
+}
+
+const parseHtmlApi = async (
+  params: ParseHtmlRequest,
+  opts: { Authorization: string }
+): Promise<ParseHtmlResponse> => {
+  const url = `${siteConfig.apiUrl}/v1/parse-html`;
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${opts?.Authorization}`,
+    },
+
+    body: JSON.stringify(params),
+  });
+  const resp = await res.json();
+
+  return resp;
+};
+
+export const useParseHtmlMutation = () => {
+  const { data: authUser } = useCurrentAuthUser({});
+
+  return useMutation({
+    mutationFn: async (params: ParseHtmlRequest) => {
+      return await parseHtmlApi(params, { Authorization: authUser?.jwt });
+    },
+  });
+};

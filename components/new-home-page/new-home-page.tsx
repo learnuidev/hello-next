62277eab8ca0e860bb21850e79c5da35e6ed55c0
learnuidev <learnuidev@gmail.com>
2026-03-31 09:17:26 -0400

    make animation snappier

--- components/new-home-page/new-home-page.tsx
+++ components/new-home-page/new-home-page.tsx
@@ -89,7 +89,7 @@ export function NewHomePage() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: -20 }}
-                transition={{ duration: 1 }}
+                transition={{ duration: 0.3 }}
               >
                 {activeTab === "series" ? (
                   <SeriesList activeTopic={activeTopic} />

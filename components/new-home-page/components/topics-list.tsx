e03daab205cfc74486faa99b74779e14aac3772b
learnuidev <learnuidev@gmail.com>
2026-03-31 06:06:08 -0400

    added topics list

---
+++ components/new-home-page/components/topics-list.tsx
@@ -0,0 +1,29 @@
+import { topicsList } from "@/domain/topic/topic.constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+export function TopicsList() {
+  const searchParams = useSearchParams();
+  const activeTopic = searchParams.get("topic");
+
+  return (
+    <section className="flex gap-12 overflow-x-auto flex-nowrap">
+      {topicsList.map((topic) => {
+        const isActive = activeTopic === topic.type;
+        return (
+          <Link
+            href={`/?topic=${topic.type}`}
+            className={cn(
+              `rounded-none hover:text-rose-500 whitespace-nowrap transition-all`,
+              isActive ? "text-rose-500 border-rose-500 pb-2 border-b-2" : ""
+            )}
+            key={topic.type}
+          >
+            {topic.title}
+          </Link>
+        );
+      })}
+    </section>
+  );
+}
--- components/new-home-page/new-home-page.tsx
+++ components/new-home-page/new-home-page.tsx
@@ -1,18 +1,13 @@
-import { topicsList } from "@/domain/topic/topic.constants";
 import { SearchBar } from "../search-bar";
+import { TopicsList } from "./components/topics-list";
 
 export function NewHomePage() {
   return (
     <div className="mx-2 sm:mx-12 mb-32">
       <SearchBar />
-      <h1>New Main Page</h1>
 
-      <main>
-        <section>
-          {topicsList.map((topic) => {
-            return <button key={topic.title}> {topic.title} </button>;
-          })}
-        </section>
+      <main className="mt-2">
+        <TopicsList />
       </main>
     </div>
   );

c42630a57be5f7a06eaba2f6d57188caf287b566
learnuidev <learnuidev@gmail.com>
2026-04-14 13:13:01 -0400

    wip

--- components/floating-character-navbar.tsx
+++ components/floating-character-navbar.tsx
@@ -159,7 +159,6 @@ export const FloatingCharacterNavbar = ({
               onOpenChange={setCollectionDialogOpen}
               characterId={characterId}
               lang={lang}
-              onAdded={handleCollectionAdded}
             />
 
             <PreviewButton />

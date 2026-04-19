2454e2a613107ff0ed1e4aaef35438b2d13658ee
learnuidev <learnuidev@gmail.com>
2026-04-19 08:48:59 -0400

    wip

--- app/(auth)/convos/[content-id]/dyna-cloze-sentence/utils/get-multi.ts
+++ app/(auth)/convos/[content-id]/dyna-cloze-sentence/utils/get-multi.ts
@@ -1,9 +1,13 @@
 // const getMulti = (val: string) => val.split('，').map(item => item.split("、")).flat()
-export const getMulti = (val: string) =>
-  val
+export const getMulti = (val: string) => {
+  if (isMulti(val)) {
+    return [val];
+  }
+  return val
     ?.split("，")
     ?.map((x) => x.split("。"))
     ?.flat();
+};
 
 export const isMulti = (val: string) => {
   if (val?.length < 24) {

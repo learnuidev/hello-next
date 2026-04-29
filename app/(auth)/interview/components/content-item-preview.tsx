b69c99cbba470c058401b4b42698fe68ab7149ad
learnuidev <learnuidev@gmail.com>
2026-04-29 09:35:09 -0400

    wip

--- app/(auth)/interview/components/content-item-preview.tsx
+++ app/(auth)/interview/components/content-item-preview.tsx
@@ -31,7 +31,10 @@ export function ContentItemPreview({
       onMouseLeave={() => setIsHovered(false)}
     >
       <button
-        onClick={() => {
+        onClick={(event) => {
+          // This prevents event from bubbling and trigger parent level events
+          event.preventDefault();
+          event.stopPropagation();
           onToggleBookmark(asset.video_id);
         }}
         className="absolute top-4 right-4 z-50 text-2xl text-white"
